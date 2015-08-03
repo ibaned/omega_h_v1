493e8615a96f49cb2921213bf127f76453f25f8d
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-02 20:07:19 -0400

    adding quality test code

---
+++ test_quality.c
@@ -0,0 +1,20 @@
+#include "quality.h"
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+  double const tri_coords[3][3] = {
+    {-1,0,0},
+    {0,sqrt(3),0},
+    {1,0,0}
+  };
+  printf("regular triangle quality = %f\n", triangle_quality(tri_coords));
+  double const tet_coords[4][3] = {
+    { 1,-1,-1},
+    { 1, 1, 1},
+    {-1, 1,-1},
+    {-1,-1, 1} 
+  };
+  printf("regular tet quality = %f\n", tet_quality(tet_coords));
+}
