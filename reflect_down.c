e061d8b3cea162857ccc5906d995382254760998
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-04 14:45:49 -0400

    reflect_down is taking dual's job

--- reflect_down.c
+++ reflect_down.c
@@ -4,6 +4,43 @@
 #include <stdlib.h>
 #include <assert.h>
 
+static unsigned copy_except(
+    unsigned const a[],
+    unsigned b[],
+    unsigned n,
+    unsigned exclude_this)
+{
+  unsigned j = 0;
+  for (unsigned i = 0; i < n; ++i)
+    if (a[i] != exclude_this)
+      b[j++] = a[i];
+  return j;
+}
+
+static unsigned has(
+    unsigned const a[],
+    unsigned n,
+    unsigned e)
+{
+  for (unsigned i = 0; i < n; ++i)
+    if (a[i] == e)
+      return 1;
+  return 0;
+}
+
+static unsigned intersect(
+    unsigned a[],
+    unsigned na,
+    unsigned const b[],
+    unsigned nb)
+{
+  unsigned j = 0;
+  for (unsigned i = 0; i < na; ++i)
+    if (has(b, nb, a[i]))
+      a[j++] = a[i];
+  return j;
+}
+
 unsigned* reflect_down(
     unsigned high_dim,
     unsigned low_dim,
@@ -20,14 +57,15 @@ unsigned* reflect_down(
   unsigned const* const* high_verts_of_lows =
     the_canonical_orders[high_dim][low_dim][0];
   for (unsigned i = 0; i < nhighs; ++i) {
-    unsigned const* up_vert = verts_of_highs + i * verts_per_high;
+    unsigned const* verts_of_high = verts_of_highs + i * verts_per_high;
     unsigned* lows_of_high = lows_of_highs + i * lows_per_high;
+    unsigned exclude_high = ((high_dim == low_dim) ? i : INVALID);
     for (unsigned j = 0; j < lows_per_high; ++j) {
       unsigned const* high_verts_of_low = high_verts_of_lows[j];
       unsigned high_buf[MAX_UP];
       unsigned high_buf_size = 0;
       for (unsigned k = 0; k < verts_per_low; ++k) {
-        unsigned vert = up_vert[high_verts_of_low[k]];
+        unsigned vert = verts_of_high[high_verts_of_low[k]];
         unsigned first_use = lows_of_verts_offsets[vert];
         unsigned end_use = lows_of_verts_offsets[vert + 1];
         if (high_buf_size)
@@ -37,10 +75,11 @@ unsigned* reflect_down(
               lows_of_verts + first_use,
               end_use - first_use);
         else
-          high_buf_size = copy(
+          high_buf_size = copy_except(
               lows_of_verts + first_use,
               high_buf,
-              end_use - first_use);
+              end_use - first_use,
+              exclude_high);
         assert(high_buf_size);
       }
       assert(high_buf_size == 1);
