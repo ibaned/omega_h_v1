2207903484f1a70a7bab6a357c81b261d53bbcf6
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-06 15:12:06 -0400

    adding function to measure all element qualities

---
+++ element_qualities.c
@@ -0,0 +1,26 @@
+#include "element_qualities.h"
+#include "tables.h"
+#include "algebra.h"
+#include "quality.h"
+#include <stdlib.h>
+
+double* element_qualities(
+    unsigned elem_dim,
+    unsigned nelems,
+    unsigned const* verts_of_elems,
+    double const* coords)
+{
+  double* out = malloc(sizeof(double) * nelems);
+  unsigned verts_per_elem = the_down_degrees[elem_dim][0];
+  quality_function qf = the_quality_functions[elem_dim];
+  for (unsigned i = 0; i < nelems; ++i) {
+    unsigned const* verts_of_elem = verts_of_elems + i * verts_per_elem;
+    double elem_x[MAX_DOWN][3];
+    for (unsigned j = 0; j < verts_per_elem; ++j) {
+      unsigned vert = verts_of_elem[j];
+      copy_vector(coords + vert * 3, elem_x[j], 3);
+    }
+    out[i] = qf(elem_x);
+  }
+  return out;
+}
---
+++ element_qualities.h
@@ -0,0 +1,10 @@
+#ifndef ELEMENT_QUALITIES_H
+#define ELEMENT_QUALITIES_H
+
+double* element_qualities(
+    unsigned elem_dim,
+    unsigned nelems,
+    unsigned const* verts_of_elems,
+    double const* coords);
+
+#endif
