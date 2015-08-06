35e662f824dfd61960da3383c39fea4cf01b1ea9
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-06 10:48:31 -0400

    test_refine_reduced coded for multiple steps

--- test_refine_reduced.c
+++ test_refine_reduced.c
@@ -7,16 +7,23 @@
 static double sf(double const x[])
 {
   (void) x;
-  return 1.0;
+  return 1.001;
 }
 
 int main()
 {
-  struct rv_mesh in = new_box_rv_mesh(3);
-  struct rv_mesh out = refine_reduced(in, sf);
-  free_rv_mesh(in);
-  printf("%u elements, %u vertices\n", out.nelems, out.nverts);
-  write_vtk("refined.vtu", out.elem_dim, out.nelems, out.nverts,
-      out.verts_of_elems, out.coords);
-  free_rv_mesh(out);
+  struct rv_mesh m = new_box_rv_mesh(2);
+  while (1) {
+    struct rv_mesh out = refine_reduced(m, sf);
+    printf("%u elements, %u vertices\n", out.nelems, out.nverts);
+    if (out.nelems == m.nelems) {
+      free_rv_mesh(out);
+      break;
+    }
+    free_rv_mesh(m);
+    m = out;
+  }
+  write_vtk("refined.vtu", m.elem_dim, m.nelems, m.nverts,
+      m.verts_of_elems, m.coords);
+  free_rv_mesh(m);
 }
