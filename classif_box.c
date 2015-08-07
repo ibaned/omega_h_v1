852b004dd8115c0253c91ad232f80ff4c41cef27
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-07 17:36:05 -0400

    cheap classification for a box

---
+++ classif_box.c
@@ -0,0 +1,23 @@
+#include "classif_box.h"
+#include <math.h>
+#include <stdlib.h>
+
+#define EPSILON 1e-10
+
+unsigned* classif_box(
+    unsigned elem_dim,
+    unsigned nverts,
+    double const* coords)
+{
+  unsigned* out = malloc(sizeof(unsigned) * nverts);
+  for (unsigned i = 0; i < nverts; ++i) {
+    double const* x = coords + i * 3;
+    unsigned classif_dim = 3;
+    for (unsigned j = 0; j < 3; ++j)
+      if ((fabs(x[j] - 0) < EPSILON) &&
+          (fabs(x[j] - 1) < EPSILON))
+        --classif_dim;
+    out[i] = classif_dim;
+  }
+  return out;
+}
---
+++ classif_box.h
@@ -0,0 +1,9 @@
+#ifndef CLASSIF_BOX_H
+#define CLASSIF_BOX_H
+
+unsigned* classif_box(
+    unsigned elem_dim,
+    unsigned nverts,
+    double const* coords);
+
+#endif
