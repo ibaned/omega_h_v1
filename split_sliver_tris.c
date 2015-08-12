1a78a74bb77947de6014ce1b48b618c85a35e907
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-12 12:17:55 -0400

    starting split_sliver_tris

---
+++ split_sliver_tris.c
@@ -0,0 +1,13 @@
+#include "split_sliver_tris.h"
+
+int split_sliver_tris(
+    unsigned elem_dim,
+    unsigned* p_nelems,
+    unsigned* p_nverts,
+    unsigned** p_verts_of_elems,
+    double** p_coords,
+    double qual_floor,
+    double edge_ratio_floor)
+{
+  return 1;
+}
---
+++ split_sliver_tris.h
@@ -0,0 +1,13 @@
+#ifndef SPLIT_SLIVER_TRIS
+#define SPLIT_SLIVER_TRIS
+
+int split_sliver_tris(
+    unsigned elem_dim,
+    unsigned* p_nelems,
+    unsigned* p_nverts,
+    unsigned** p_verts_of_elems,
+    double** p_coords,
+    double qual_floor,
+    double edge_ratio_floor);
+
+#endif
