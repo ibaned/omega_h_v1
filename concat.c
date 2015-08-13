ibaned/omega_h_v1

commit ffca096ca3148bf8f47d16233ae35c89e8a99777

restrict concat to only two arrays

concat.c
@@ -5,43 +5,39 @@
 
 static void* concat_general(
     unsigned typesize,
-    unsigned narrs,
     unsigned width,
-    unsigned const* sizes,
-    void const* const* arrs)
+    void const* a,
+    unsigned na,
+    void const* b,
+    unsigned nb)
 {
-  unsigned total_bytes = 0;
-  for (unsigned i = 0; i < narrs; ++i)
-    total_bytes += sizes[i];
-  total_bytes *= width * typesize;
-  void* out = malloc(total_bytes);
-  char* p = out;
-  for (unsigned i = 0; i < narrs; ++i) {
-    unsigned arr_bytes = sizes[i] * width * typesize;
-    memcpy(p, arrs[i], arr_bytes);
-    p += arr_bytes;
-  }
+  unsigned a_bytes = na * width * typesize;
+  unsigned b_bytes = na * width * typesize;
+  unsigned total_bytes = a_bytes + b_bytes;
+  char* out = malloc(total_bytes);
+  memcpy(out, a, a_bytes);
+  memcpy(out + a_bytes, b, b_bytes);
   return out;
 }
 
 unsigned* concat_ints(
-    unsigned narrs,
     unsigned width,
-    unsigned const* sizes,
-    unsigned const* const* arrs)
+    unsigned const a[],
+    unsigned na,
+    unsigned const b[],
+    unsigned nb)
 {
-  return concat_general(sizeof(unsigned), narrs, width, sizes,
-      (void const* const*) arrs);
+  return concat_general(sizeof(unsigned), width, a, na, b, nb);
 }
 
 double* concat_doubles(
-    unsigned narrs,
     unsigned width,
-    unsigned const* sizes,
-    double const* const* arrs)
+    double const a[],
+    unsigned na,
+    double const b[],
+    unsigned nb)
 {
-  return concat_general(sizeof(double), narrs, width, sizes,
-      (void const* const*) arrs);
+  return concat_general(sizeof(double), width, a, na, b, nb);
 }
 
 void* general_subset(
@@ -97,9 +93,9 @@ void concat_verts_of_elems(
   unsigned nsame_elems = offset_of_same_elems[nelems];
   unsigned* verts_of_same_elems = ints_subset(nelems, verts_per_elem,
       verts_of_elems, offset_of_same_elems);
-  unsigned sizes[2] = { nsame_elems, ngen_elems };
-  unsigned const* arrs[2] = { verts_of_same_elems, verts_of_gen_elems };
-  unsigned* out = concat_ints(2, verts_per_elem, sizes, arrs);
+  unsigned* out = concat_ints(verts_per_elem,
+      verts_of_same_elems, nsame_elems,
+      verts_of_gen_elems, ngen_elems);
   free(verts_of_same_elems);
   *nelems_out = nsame_elems + ngen_elems;
   *verts_of_elems_out = out;

concat.h
@@ -14,16 +14,18 @@ double* doubles_subset(
     unsigned const* offsets);
 
 unsigned* concat_ints(
-    unsigned narrs,
     unsigned width,
-    unsigned const* sizes,
-    unsigned const* const* arrs);
+    unsigned const a[],
+    unsigned na,
+    unsigned const b[],
+    unsigned nb);
 
 double* concat_doubles(
-    unsigned narrs,
     unsigned width,
-    unsigned const* sizes,
-    double const* const* arrs);
+    double const a[],
+    unsigned na,
+    double const b[],
+    unsigned nb);
 
 void concat_verts_of_elems(
     unsigned elem_dim,

refine_common.c
@@ -61,17 +61,15 @@ void refine_common(
   double* gen_coords = refine_nodal(src_dim, nsrcs, verts_of_srcs,
       gen_offset_of_srcs, 3, coords);
   free(gen_offset_of_srcs);
-  unsigned concat_sizes[2] = {nverts, nsplit_srcs};
-  unsigned nverts_out = nverts + nsplit_srcs;
-  double const* concat_coords[2] = {coords, gen_coords};
-  double* coords_out = concat_doubles(2, 3, concat_sizes, concat_coords);
+  double* coords_out = concat_doubles(3, coords, nverts,
+      gen_coords, nsplit_srcs);
   free(gen_coords);
   if (p_class_dim) {
     unsigned const* class_dim = *p_class_dim;
     unsigned* gen_class_dim = refine_classif(src_dim, nsrcs, verts_of_srcs,
         gen_offset_of_srcs, class_dim);
-    unsigned const* ccd[2] = {class_dim, gen_class_dim};
-    unsigned* class_dim_out = concat_ints(2, 1, concat_sizes, ccd);
+    unsigned* class_dim_out = concat_ints(1, class_dim, nverts,
+        gen_class_dim, nsplit_srcs);
     free(gen_class_dim);
     free(*p_class_dim);
     *p_class_dim = class_dim_out;
@@ -87,7 +85,7 @@ void refine_common(
   free(offset_of_same_elems);
   free(verts_of_gen_elems);
   *p_nelems = nelems_out;
-  *p_nverts = nverts_out;
+  *p_nverts += nsplit_srcs;
   free(*p_verts_of_elems);
   *p_verts_of_elems = verts_of_elems_out;
   free(*p_coords);

commit d48cb4c4ce8d4f443d59a18cc272863f77beb469

fix typo introduced by ffca096c

concat.c
@@ -12,7 +12,7 @@ static void* concat_general(
     unsigned nb)
 {
   unsigned a_bytes = na * width * typesize;
-  unsigned b_bytes = na * width * typesize;
+  unsigned b_bytes = nb * width * typesize;
   unsigned total_bytes = a_bytes + b_bytes;
   char* out = malloc(total_bytes);
   memcpy(out, a, a_bytes);