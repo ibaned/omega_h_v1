01f0d00b46472d2606005a86a74a27bcbeccbc4d
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-24 13:50:10 -0400

    print other fields in vtk form

--- vtk.c
+++ vtk.c
@@ -16,60 +16,74 @@ static unsigned const vtk_types[4] = {
  * (the VTK format supports it)
  */
 
+static void write_nodal_field(FILE* file, struct mesh* m, struct const_field* field)
+{
+  fprintf(file, "<DataArray type=\"Float64\" Name=\"%s\""
+             " NumberOfComponents=\"%u\" format=\"ascii\">\n",
+             field->name, field->ncomps);
+  unsigned nverts = mesh_count(m, 0);
+  double const* p = field->data;
+  for (unsigned i = 0; i < nverts; ++i) {
+    for (unsigned j = 0; j < field->ncomps; ++j)
+      fprintf(file, " %f", *p++);
+    fprintf(file, "\n");
+  }
+  fprintf(file, "</DataArray>\n");
+}
+
 void write_vtk(struct mesh* m, char const* filename)
 {
   unsigned elem_dim = mesh_dim(m);
   unsigned nverts = mesh_count(m, 0);
   unsigned nelems = mesh_count(m, elem_dim);
   unsigned const* verts_of_elems = mesh_ask_down(m, elem_dim, 0);
-  double const* coords = mesh_find_nodal_field(m, "coordinates")->data;
-  FILE* f = fopen(filename, "w");
-  fprintf(f, "<VTKFile type=\"UnstructuredGrid\">\n");
-  fprintf(f, "<UnstructuredGrid>\n");
-  fprintf(f, "<Piece NumberOfPoints=\"%u\" NumberOfCells=\"%u\">\n", nverts, nelems);
-  fprintf(f, "<Points>\n");
-  fprintf(f, "<DataArray type=\"Float64\" Name=\"coordinates\""
-             " NumberOfComponents=\"3\" format=\"ascii\">\n");
-  for (unsigned i = 0; i < nverts; ++i) {
-    double const* p = coords + i * 3;
-    fprintf(f, "%f %f %f\n", p[0], p[1], p[2]);
-  }
-  fprintf(f, "</DataArray>\n");
-  fprintf(f, "</Points>\n");
-  fprintf(f, "<Cells>\n");
-  fprintf(f, "<DataArray type=\"UInt32\" Name=\"connectivity\" format=\"ascii\">\n");
+  FILE* file = fopen(filename, "w");
+  fprintf(file, "<VTKFile type=\"UnstructuredGrid\">\n");
+  fprintf(file, "<UnstructuredGrid>\n");
+  fprintf(file, "<Piece NumberOfPoints=\"%u\" NumberOfCells=\"%u\">\n", nverts, nelems);
+  fprintf(file, "<Points>\n");
+  struct const_field* coord_field = mesh_find_nodal_field(m, "coordinates");
+  write_nodal_field(file, m, coord_field);
+  fprintf(file, "</Points>\n");
+  fprintf(file, "<Cells>\n");
+  fprintf(file, "<DataArray type=\"UInt32\" Name=\"connectivity\" format=\"ascii\">\n");
   unsigned down_degree = the_down_degrees[elem_dim][0];
   for (unsigned i = 0; i < nelems; ++i) {
     unsigned const* p = verts_of_elems + i * down_degree;
     for (unsigned j = 0; j < down_degree; ++j)
-      fprintf(f, " %u", p[j]);
-    fprintf(f, "\n");
+      fprintf(file, " %u", p[j]);
+    fprintf(file, "\n");
   }
-  fprintf(f, "</DataArray>\n");
-  fprintf(f, "<DataArray type=\"UInt32\" Name=\"offsets\" format=\"ascii\">\n");
+  fprintf(file, "</DataArray>\n");
+  fprintf(file, "<DataArray type=\"UInt32\" Name=\"offsets\" format=\"ascii\">\n");
   for (unsigned i = 0; i < nelems; ++i)
-    fprintf(f, "%u\n", (i + 1) * down_degree);
-  fprintf(f, "</DataArray>\n");
-  fprintf(f, "<DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n");
+    fprintf(file, "%u\n", (i + 1) * down_degree);
+  fprintf(file, "</DataArray>\n");
+  fprintf(file, "<DataArray type=\"UInt8\" Name=\"types\" format=\"ascii\">\n");
   unsigned type = vtk_types[elem_dim];
   for (unsigned i = 0; i < nelems; ++i)
-    fprintf(f, "%u\n", type);
-  fprintf(f, "</DataArray>\n");
-  fprintf(f, "</Cells>\n");
-  fprintf(f, "<PointData>\n");
+    fprintf(file, "%u\n", type);
+  fprintf(file, "</DataArray>\n");
+  fprintf(file, "</Cells>\n");
+  fprintf(file, "<PointData>\n");
   if (mesh_find_nodal_label(m, "class_dim")) {
     unsigned const* class_dim = mesh_find_nodal_label(m, "class_dim")->data;
-    fprintf(f, "<DataArray type=\"UInt32\" Name=\"class_dim\""
+    fprintf(file, "<DataArray type=\"UInt32\" Name=\"class_dim\""
                " NumberOfComponents=\"1\" format=\"ascii\">\n");
     for (unsigned i = 0; i < nverts; ++i)
-      fprintf(f, "%u\n", class_dim[i]);
-    fprintf(f, "</DataArray>\n");
+      fprintf(file, "%u\n", class_dim[i]);
+    fprintf(file, "</DataArray>\n");
   }
-  fprintf(f, "</PointData>\n");
-  fprintf(f, "<CellData>\n");
-  fprintf(f, "</CellData>\n");
-  fprintf(f, "</Piece>\n");
-  fprintf(f, "</UnstructuredGrid>\n");
-  fprintf(f, "</VTKFile>\n");
-  fclose(f);
+  for (unsigned i = 0; i < mesh_count_nodal_fields(m); ++i) {
+    struct const_field* field = mesh_get_nodal_field(m, i);
+    if (field != coord_field)
+      write_nodal_field(file, m, field);
+  }
+  fprintf(file, "</PointData>\n");
+  fprintf(file, "<CellData>\n");
+  fprintf(file, "</CellData>\n");
+  fprintf(file, "</Piece>\n");
+  fprintf(file, "</UnstructuredGrid>\n");
+  fprintf(file, "</VTKFile>\n");
+  fclose(file);
 }
