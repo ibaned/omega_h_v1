50548426b07c6b5f3df8ca7d2c8b49ca2d4c9add
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-25 14:33:01 -0400

    gradient of quadratic looks linear

--- test_grad.c
+++ test_grad.c
@@ -1,16 +1,23 @@
 #include "mesh.h"
 #include "eval_field.h"
 #include "element_gradients.h"
+#include "refine_by_size.h"
 #include "vtk.h"
 
 static void fld_fun(double const x[3], double v[])
 {
-  v[0] = x[0] * 5 + x[1] * 2 + x[2] * -3;
+  v[0] = x[0] * x[0];
+}
+
+static double size_fun(double const x[])
+{
+  return 0.1;
 }
 
 int main()
 {
-  struct mesh* m = new_box_mesh(3);
+  struct mesh* m = new_box_mesh(2);
+  while (refine_by_size(&m, size_fun));
   mesh_eval_field(m, "foo", 1, fld_fun);
   mesh_element_gradients(m, "foo");
   write_vtk(m, "grad.vtu");
