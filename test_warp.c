8dca508f5127639a9c0535be1cc35b39b9b78cf2
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-25 09:17:28 -0400

    can advect a dye field !

--- test_warp.c
+++ test_warp.c
@@ -15,10 +15,10 @@
 
 static double size_fun(double const x[])
 {
-  return 0.2;
+  return 0.1;
 }
 
-static double const max_rot = M_PI / 4.;
+static double the_rotation = M_PI / 4.;
 
 static void warp_fun(double const coords[3], double v[])
 {
@@ -27,7 +27,7 @@ static void warp_fun(double const coords[3], double v[])
   subtract_vectors(coords, mid, x, 3);
   double polar_a = atan2(x[1], x[0]);
   double polar_r = vector_norm(x, 3);
-  double rot_a = max_rot * (2 * (.5 - polar_r));
+  double rot_a = the_rotation * (2 * (.5 - polar_r));
   if (rot_a < 0)
     rot_a = 0;
   double dest_a = polar_a + rot_a;
@@ -38,32 +38,75 @@ static void warp_fun(double const coords[3], double v[])
   subtract_vectors(dst, x, v, 3);
 }
 
+static void dye_fun(double const coords[3], double v[])
+{
+  double x[3];
+  double const l[3] = {.25, .5, 0};
+  double const r[3] = {.75, .5, 0};
+  double dir = 1;
+  subtract_vectors(coords, l, x, 3);
+  if (vector_norm(x, 3) > .25) {
+    dir = -1;
+    subtract_vectors(coords, r, x, 3);
+  }
+  if (vector_norm(x, 3) > .25) {
+    v[0] = 0;
+    return;
+  }
+  v[0] = 4 * dir * (.25 - vector_norm(x, 3));
+}
+
+static void warped_adapt(struct mesh** p_m)
+{
+  struct mesh* m = *p_m;
+  unsigned done_warp = 0;
+  for (unsigned j = 0; j < 40; ++j) {
+    if (!done_warp) {
+      done_warp = mesh_warp_to_limit(m, 0.1);
+      printf("warp\n");
+      write_vtk_step(m);
+    }
+    unsigned did_refine =
+      refine_by_size(&m, size_fun);
+    if (did_refine) {
+      printf("refine\n");
+      write_vtk_step(m);
+    }
+    unsigned did_sliver =
+      split_sliver_tris(&m, 0.4, 1.0 / 5.0);
+    if (did_sliver) {
+      printf("sliver\n");
+      write_vtk_step(m);
+    }
+    unsigned did_coarsen =
+      coarsen_by_size(&m, size_fun,
+          mesh_min_quality(m), 1.0 / 3.0);
+    if (did_coarsen) {
+      printf("coarsen\n");
+      write_vtk_step(m);
+    }
+    if (done_warp && !did_refine && !did_sliver && !did_coarsen) {
+      *p_m = m;
+      return;
+    }
+  }
+  printf("40 operations couldn't converge !\n");
+  abort();
+}
+
 int main()
 {
   struct mesh* m = new_box_mesh(2);
   while (refine_by_size(&m, size_fun));
   mesh_classify_box(m);
   start_vtk_steps("warp");
-  write_vtk_step(m);
-  while (1) {
+  mesh_eval_field(m, "dye", 1, dye_fun);
+  for (unsigned j = 0; j < 4; ++j) {
     mesh_eval_field(m, "warp", 3, warp_fun);
-    unsigned ok = mesh_warp_to_limit(m, 0.1);
+    printf("new warp field\n");
+    write_vtk_step(m);
+    warped_adapt(&m);
     mesh_free_nodal_field(m, "warp");
-    write_vtk_step(m);
-    if (!ok)
-      break;
-  }
-  while (1) {
-    unsigned did_stuff = 0;
-    did_stuff |= coarsen_by_size(&m, size_fun,
-        mesh_min_quality(m), 1.0 / 3.0);
-    write_vtk_step(m);
-    did_stuff |= refine_by_size(&m, size_fun);
-    write_vtk_step(m);
-    did_stuff |= split_sliver_tris(&m, 0.4, 1.0 / 5.0);
-    write_vtk_step(m);
-    if (!did_stuff)
-      break;
   }
   free_mesh(m);
 }
