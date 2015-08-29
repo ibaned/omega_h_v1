cfd6edcd13c97aee0e04671e361fe84845b68b53
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-29 18:12:40 -0400

    more 3D advection work. tri slivers seem gone

--- test_warp_3d.c
+++ test_warp_3d.c
@@ -28,6 +28,7 @@ static void warp_fun(double const coords[3], double v[])
   double x[3];
   double const mid[3] = {.5, .5, 0};
   subtract_vectors(coords, mid, x, 3);
+  x[2] = 0;
   double polar_a = atan2(x[1], x[0]);
   double polar_r = vector_norm(x, 3);
   double rot_a = 0;
@@ -37,7 +38,7 @@ static void warp_fun(double const coords[3], double v[])
   double dst[3];
   dst[0] = cos(dest_a) * polar_r;
   dst[1] = sin(dest_a) * polar_r;
-  dst[2] = x[2];
+  dst[2] = 0;
   subtract_vectors(dst, x, v, 3);
 }
 
@@ -59,6 +60,7 @@ static void dye_fun(double const coords[3], double v[])
   v[0] = 4 * dir * (.25 - vector_norm(x, 3));
 }
 
+/*
 static void set_size_field(struct mesh* m)
 {
   mesh_free_nodal_field(m, "adapt_size");
@@ -73,12 +75,13 @@ static void set_size_field(struct mesh* m)
   mesh_size_from_hessian(m, "rcov_grad_rcov_grad_dye", &weight, 0.05, 0.1);
   mesh_free_nodal_field(m, "rcov_grad_rcov_grad_dye");
 }
+*/
 
 static void adapt(struct mesh** p_m)
 {
   struct mesh* m = *p_m;
-  set_size_field(m);
-  write_vtk_step(m);
+//set_size_field(m);
+//write_vtk_step(m);
   while (refine_by_size(&m)) {
     printf("refine\n");
     write_vtk_step(m);
@@ -87,6 +90,7 @@ static void adapt(struct mesh** p_m)
     printf("coarsen\n");
     write_vtk_step(m);
   }
+  printf("min tet qual %f\n", mesh_min_quality(m));
   while (split_slivers(2, &m, 0.4, 1.0 / 5.0)) {
     printf("sliver\n");
     write_vtk_step(m);
@@ -94,6 +98,7 @@ static void adapt(struct mesh** p_m)
     printf("sliver coarsen\n");
     write_vtk_step(m);
   }
+  printf("min tet qual %f\n", mesh_min_quality(m));
   *p_m = m;
 }
 
@@ -102,6 +107,7 @@ static void warped_adapt(struct mesh** p_m)
   unsigned done = 0;
   do {
     done = mesh_warp_to_limit(*p_m, 0.1);
+    write_vtk_step(*p_m);
     adapt(p_m);
   } while (!done);
 }
@@ -114,15 +120,16 @@ int main()
   mesh_classify_box(m);
   start_vtk_steps("warp");
   mesh_eval_field(m, "dye", 1, dye_fun);
+  write_vtk_step(m);
   adapt(&m);
-//for (unsigned i = 0; i < 0; ++i) {
-//  for (unsigned j = 0; j < 0; ++j) {
-//    mesh_eval_field(m, "warp", 3, warp_fun);
-//    printf("new warp field\n");
-//    warped_adapt(&m);
-//    mesh_free_nodal_field(m, "warp");
-//  }
-//  the_rotation = -the_rotation;
-//}
+  for (unsigned i = 0; i < 1; ++i) {
+    for (unsigned j = 0; j < 2; ++j) {
+      mesh_eval_field(m, "warp", 3, warp_fun);
+      printf("new warp field\n");
+      warped_adapt(&m);
+      mesh_free_nodal_field(m, "warp");
+    }
+    the_rotation = -the_rotation;
+  }
   free_mesh(m);
 }
