ea9b7904a9c1c7c375830fb5b1d18d9d8b05f87e
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-29 13:35:57 -0400

    do use the in-plane assumption to limit warps

--- quality.c
+++ quality.c
@@ -219,7 +219,7 @@ double* element_qualities(
 {
   double* out = malloc(sizeof(double) * nelems);
   unsigned verts_per_elem = the_down_degrees[elem_dim][0];
-  quality_function qf = the_quality_functions[elem_dim];
+  quality_function qf = the_equal_order_quality_functions[elem_dim];
   for (unsigned i = 0; i < nelems; ++i) {
     unsigned const* verts_of_elem = verts_of_elems + i * verts_per_elem;
     double elem_x[MAX_DOWN][3];
