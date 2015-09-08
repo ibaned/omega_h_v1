af31e166d2ab980f24b33fcbe0f41282f8c79d62
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-08 15:20:24 -0400

    going back to previous 3D parameters

--- test_warp_3d.c
+++ test_warp_3d.c
@@ -15,8 +15,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static double const warp_qual_floor = 0.1;
-static double const good_qual_floor = 0.2;
+static double const warp_qual_floor = 0.2;
+static double const good_qual_floor = 0.25;
 static double const size_floor = 1. / 3.;
 
 static void size_fun(double const x[], double s[])
