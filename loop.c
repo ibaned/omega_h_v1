cc0c474ec7f65cdec5efbc4d0e46156b6c52e5ad
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-14 19:43:29 -0400

    specify malloc/realloc behavior with zero size
    note that this affects the checks we just put
    in place, i.e. a zero pointer is now returned
    for something which is "allocated", but that
    is a rare enough case that I'd rather not
    introduce a bunch of variables just to detect
    it. those are asserts, not production logic.

--- loop.c
+++ loop.c
@@ -23,7 +23,15 @@ void* loop_host_malloc(unsigned long n)
 
 void* loop_host_realloc(void* p, unsigned long n)
 {
-  return realloc(p, n);
+  if (!n) {
+    free(p);
+    return NULL;
+  }
+  if (!p)
+    return loop_host_malloc(n);
+  void* q = realloc(p, n);
+  assert(q);
+  return q;
 }
 
 void loop_host_free(void* p)
