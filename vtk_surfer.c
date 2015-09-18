dec6c2e6244163bf8c058c6d38459a2ea5dfa01b
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-18 10:06:02 -0400

    fixed bugs in surfer

--- vtk_surfer.c
+++ vtk_surfer.c
@@ -3,16 +3,21 @@
 #include "mark.h"
 #include "subset.h"
 #include "loop.h"
+#include "ints.h"
 #include <assert.h>
 
 int main(int argc, char** argv)
 {
   assert(argc == 3);
   struct mesh* m = read_vtk(argv[1]);
+  unsigned d = mesh_dim(m);
+  unsigned ns = mesh_count(m, d - 1);
   unsigned* pb = mesh_mark_part_boundary(m);
-  struct mesh* sm = subset_mesh(m, mesh_dim(m) - 1, pb);
-  free_mesh(m);
+  unsigned* off = ints_exscan(pb, ns);
   loop_free(pb);
+  struct mesh* sm = subset_mesh(m, d - 1, off);
+  free_mesh(m);
+  loop_free(off);
   write_vtk(sm, argv[2]);
   free_mesh(sm);
 }
