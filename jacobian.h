d330491c68b7c787f49bdcce32ba289dac98451c
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-20 18:13:12 -0400

    comment on Jacobian definition

--- jacobian.h
+++ jacobian.h
@@ -1,7 +1,19 @@
 #ifndef JACOBIAN_H
 #define JACOBIAN_H
 
-static inline void element_jacobian(unsigned nv, double x[][3], double jac[3][3])
+/* we define the element Jacobian such that
+      J \xi + x_0 = x
+   where J is the Jacobian, \xi is the column
+   vector of parent or reference space coordinates,
+   x_0 is the spatial location of the first vertex,
+   and x is the resulting spacial location corresponding
+   to \xi.
+   This means the Jacobian ends up being:
+     J = [ x_1 - x_0 | x_2 - x_0 | x_3 - x_0 ]
+ */
+
+static inline void element_jacobian(unsigned nv, double x[][3],
+    double jac[3][3])
 {
   for (unsigned i = 0; i < 3; ++i)
   for (unsigned j = 0; j < 3; ++j)
