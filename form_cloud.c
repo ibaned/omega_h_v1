814774c1cefdc4c65846703019195aa7d712b105
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-24 18:13:01 -0400

    finish small tests of form_cloud

--- form_cloud.c
+++ form_cloud.c
@@ -7,8 +7,6 @@
 #include <math.h>
 #include <assert.h>
 
-#include <stdio.h>
-
 static void form_elem_cloud_2d(
     unsigned elem,
     unsigned const* elem_pt_offsets,
@@ -17,17 +15,13 @@ static void form_elem_cloud_2d(
     double* coords_of_pts,
     unsigned* elem_of_pts)
 {
-  fprintf(stderr, "forming cloud for element %u\n", elem);
   unsigned elem_pts = (elem_pt_offsets[elem + 1]
                      - elem_pt_offsets[elem]);
-  fprintf(stderr, "%u points in this element\n", elem_pts);
   if (!elem_pts)
     return;
   unsigned res;
   for (res = 1; ((res * (res + 1)) / 2) < elem_pts; ++res);
-  fprintf(stderr, "resolution %u\n", res);
   double dxi = 1.0 / (res + 2);
-  fprintf(stderr, "dxi %f\n", dxi);
   unsigned elem_pt = 0;
   unsigned pt_offset = elem_pt_offsets[elem];
   double const* elem_x[3];
@@ -38,12 +32,10 @@ static void form_elem_cloud_2d(
   for (unsigned j = 0; j < (res - i); ++j) {
     if (elem_pt == elem_pts)
       return;
-    fprintf(stderr, "local point %u\n", elem_pt);
     double xi[3];
     xi[0] = (i + 1) * dxi;
     xi[1] = (j + 1) * dxi;
     xi[2] = 1.0 - xi[0] - xi[1];
-    fprintf(stderr, "parent coordinates %f %f %f\n", xi[0], xi[1], xi[2]);
     double* coords_of_pt = coords_of_pts + (pt_offset + elem_pt) * 3;
     for (unsigned k = 0; k < 3; ++k)
       coords_of_pt[k] = 0;
@@ -62,19 +54,15 @@ struct cloud* form_cloud(struct mesh* m)
   double const* elem_density = mesh_find_tag(m, dim, "cloud_density")->data;
   double const* elem_size = mesh_element_sizes(m);
   double* pts_of_elems = loop_malloc(sizeof(double) * nelems);
-  for (unsigned i = 0; i < nelems; ++i) {
+  for (unsigned i = 0; i < nelems; ++i)
     pts_of_elems[i] = elem_size[i] * elem_density[i];
-    fprintf(stderr, "%f real points for element %u\n", pts_of_elems[i], i);
-  }
   double* real_offset = doubles_exscan(pts_of_elems, nelems);
   loop_free(pts_of_elems);
   unsigned* uint_offset = loop_malloc(sizeof(unsigned) * (nelems + 1));
-  for (unsigned i = 0; i <= nelems; ++i) {
+  for (unsigned i = 0; i <= nelems; ++i)
     uint_offset[i] = (unsigned) (floor(real_offset[i]));
-  }
   loop_free(real_offset);
   unsigned npts = uint_offset[nelems];
-  fprintf(stderr, "%u total cloud points\n", npts);
   struct cloud* c = new_cloud(npts);
   unsigned* pt_elem = loop_malloc(sizeof(unsigned) * npts);
   double* pt_coords = loop_malloc(sizeof(double) * npts * 3);
