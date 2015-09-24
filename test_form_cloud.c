43990c6e4366df13475556b35c4146a72e154a9b
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-24 18:39:20 -0400

    "real" test of form_cloud looks good

--- test_form_cloud.c
+++ test_form_cloud.c
@@ -3,14 +3,25 @@
 #include "vtk.h"
 #include "loop.h"
 #include "cloud.h"
+#include "element_field.h"
+#include "eval_field.h"
+#include "algebra.h"
+
+static void density_fun(double const x[3], double* d)
+{
+  static double c[3] = {0.9, 0, 0};
+  double r = vector_distance(x, c, 3);
+  if (0.3 < r && r < 0.45)
+    *d = 1000;
+  else
+    *d = 0;
+}
 
 int main()
 {
-  struct mesh* m = new_box_mesh(2);
-  double* density = loop_malloc(sizeof(double) * 2);
-  density[0] = 6.1;
-  density[1] = 6.1;
-  mesh_add_tag(m, 2, TAG_F64, "cloud_density", 1, density);
+  struct mesh* m = read_vtk("xgc.vtu");
+  mesh_interp_to_elems(m, "coordinates");
+  mesh_eval_field(m, 2, "cloud_density", 1, density_fun);
   struct cloud* c = form_cloud(m);
   write_vtk(m, "cloud_mesh.vtu");
   write_vtk_cloud(c, "cloud.vtu");
