6fa7830997333c072db467b511b3b4b4fe923552
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-09-27 15:42:01 -0400

    more work on push test

--- test_push_cloud.c
+++ test_push_cloud.c
@@ -5,11 +5,18 @@
 #include "element_field.h"
 #include "eval_field.h"
 #include "vtk.h"
+#include "algebra.h"
 
-static void density_fun(double const x[3], double* d)
+static void density_fun(double const* x, double* d)
 {
   (void) x;
   *d = 2.1;
+}
+
+static void new_coord_fun(double const* x, double* nx)
+{
+  double const v[3] = {-.4, .4, 0};
+  add_vectors(x, v, nx, 3);
 }
 
 int main()
@@ -20,6 +27,11 @@ int main()
   struct cloud * c = form_cloud(m);
   write_vtk(m, "mesh.vtu");
   write_vtk_cloud(c, "cloud_0.vtu");
+  cloud_eval_field(c, "new_coords", 3, new_coord_fun);
+  write_vtk_cloud(c, "cloud_1.vtu");
+  cloud_free_tag(c, "coordinates");
+  cloud_rename_tag(c, "new_coords", "coordinates");
+  write_vtk_cloud(c, "cloud_2.vtu");
   free_cloud(c);
   free_mesh(m);
 }
