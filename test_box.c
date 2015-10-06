c40729e3c95cf5fb059bd4ce4a72e65ecc6f06e1
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-06 15:13:40 -0400

    several fixes to box.exe

--- test_box.c
+++ test_box.c
@@ -7,10 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static double size = 0;
+static double size = 0.75;
 
-void sizefun(double const* x, double* o)
+static void sizefun(double const* x, double* o)
 {
+  (void) x;
   *o = size;
 }
 
@@ -18,7 +19,6 @@ int main(int argc, char** argv)
 {
   unsigned dim = 3;
   char const* file = "out.vtu";
-  unsigned adapt = 0;
   for (int i = 0; i < argc; ++i) {
     if (!strcmp(argv[i], "--dim")) {
       ++i;
@@ -26,14 +26,13 @@ int main(int argc, char** argv)
         printf("--dim needs an argument\n");
         return -1;
       }
-      dim = atoi(argv[i]);
+      dim = (unsigned) atoi(argv[i]);
     } else if (!strcmp(argv[i], "--size")) {
       ++i;
       if (i == argc) {
         printf("--size needs an argument\n");
         return -1;
       }
-      adapt = 1;
       size = atof(argv[i]);
     } else if (!strcmp(argv[i], "--file")) {
       ++i;
@@ -45,11 +44,9 @@ int main(int argc, char** argv)
     }
   }
   struct mesh* m = new_box_mesh(dim);
-  if (adapt) {
-    mesh_eval_field(m, 0, "adapt_size", 1, sizefun);
-    while (refine_by_size(&m, 0.0));
-    mesh_free_tag(m, 0, "adapt_size");
-  }
+  mesh_eval_field(m, 0, "adapt_size", 1, sizefun);
+  while (refine_by_size(&m, 0.0));
+  mesh_free_tag(m, 0, "adapt_size");
   mesh_classify_box(m);
   write_vtk(m, file);
   free_mesh(m);
