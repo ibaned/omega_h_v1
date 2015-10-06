5fbcdbd4192fbd842d3ca35a584c6acc171a3b99
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-06 18:44:49 -0400

    changes for Sandia users

--- include/omega_h.h
+++ include/omega_h.h
@@ -3,9 +3,24 @@
 
 typedef struct osh_mesh* osh_t;
 
-osh_t osh_read_vtk(char const* filename);
-unsigned const* osh_ask_down(osh_t m, unsigned high_dim, unsigned low_dim);
-double const* osh_ask_coords(osh_t m);
 void osh_free(osh_t m);
+
+osh_t osh_read_vtk(char const* filename);
+void osh_write_vtk(osh_t m, char const* filename);
+
+unsigned osh_dim(osh_t m);
+unsigned osh_nelems(osh_t m);
+unsigned osh_nverts(osh_t m);
+
+unsigned const* osh_down(osh_t m, unsigned high_dim, unsigned low_dim);
+unsigned const* osh_up(osh_t m, unsigned low_dim, unsigned high_dim);
+unsigned const* osh_up_offs(osh_t m, unsigned low_dim, unsigned high_dim);
+unsigned const* osh_up_dirs(osh_t m, unsigned low_dim, unsigned high_dim);
+
+double const* osh_coords(osh_t m);
+
+void osh_set_field(osh_t m, char const* name, unsigned ncomps, double* data);
+
+unsigned const* osh_ask_label(osh_t m, char const* name);
 
 #endif
