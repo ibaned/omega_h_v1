9ca36a907490955b305873ea19851344c8367db1
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-14 16:01:12 -0400

    actually add the files...

---
+++ base64.h
@@ -0,0 +1,8 @@
+#ifndef BASE_64_H
+#define BASE_64_H
+
+char* base64_encode(void const* data, unsigned long size);
+
+void print_base64_reverse(void);
+
+#endif
---
+++ test_base64.c
@@ -0,0 +1,6 @@
+#include "base64.h"
+
+int main()
+{
+  print_base64_reverse();
+}
