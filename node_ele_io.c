30e405fda9becfc6f2a051d9b366ddacb4f955fa
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-16 21:01:39 -0400

    using safe_scanf in node_ele_io

--- node_ele_io.c
+++ node_ele_io.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include "files.h"
 #include "loop.h"
 #include "mesh.h"
 #include "tag.h"
@@ -11,7 +12,7 @@ struct mesh* read_dot_node(char const* filename)
 {
   FILE* f = fopen(filename, "r");
   unsigned nnodes, dim, nattrib, nbdrys;
-  fscanf(f, "%u %u %u %u", &nnodes, &dim, &nattrib, &nbdrys);
+  safe_scanf(f, 4, "%u %u %u %u", &nnodes, &dim, &nattrib, &nbdrys);
   assert(nnodes < 100 * 1000 * 1000);
   assert(nattrib < 100);
   struct mesh* m = new_mesh(dim);
@@ -24,16 +25,16 @@ struct mesh* read_dot_node(char const* filename)
   if (nbdrys)
     bdry = LOOP_HOST_MALLOC(unsigned, nnodes);
   for (unsigned i = 0; i < nnodes; ++i) {
-    fscanf(f, "%*u");
+    safe_scanf(f, 0, "%*u");
     unsigned j;
     for (j = 0; j < dim; ++j)
-      fscanf(f, "%lf", coords + i * 3 + j);
+      safe_scanf(f, 1, "%lf", coords + i * 3 + j);
     for (; j < 3; ++j)
       coords[i * 3 + j] = 0;
     for (j = 0; j < nattrib; ++j)
-      fscanf(f, "%lf", attrib + i * nattrib + j);
+      safe_scanf(f, 1, "%lf", attrib + i * nattrib + j);
     if (nbdrys)
-      fscanf(f, "%u", bdry + i);
+      safe_scanf(f, 1, "%u", bdry + i);
   }
   fclose(f);
   mesh_set_ents(m, 0, nnodes, 0);
@@ -86,7 +87,7 @@ void read_dot_ele(struct mesh* m, char const* filename)
 {
   unsigned nelems, verts_per_elem, nattrib;
   FILE* f = fopen(filename, "r");
-  fscanf(f, "%u %u %u", &nelems, &verts_per_elem, &nattrib);
+  safe_scanf(f, 3, "%u %u %u", &nelems, &verts_per_elem, &nattrib);
   assert(nelems < 100 * 1000 * 1000);
   assert(nattrib < 100);
   assert(verts_per_elem >= 2);
@@ -99,14 +100,14 @@ void read_dot_ele(struct mesh* m, char const* filename)
   if (nattrib)
     attrib = LOOP_HOST_MALLOC(double, nelems * nattrib);
   for (unsigned i = 0; i < nelems; ++i) {
-    fscanf(f, "%*u");
+    safe_scanf(f, 0, "%*u");
     for (unsigned j = 0; j < verts_per_elem; ++j) {
       unsigned v;
-      fscanf(f, "%u", &v);
+      safe_scanf(f, 1, "%u", &v);
       verts_of_elems[i * verts_per_elem + j] = v - 1;
     }
     for (unsigned j = 0; j < nattrib; ++j)
-      fscanf(f, "%lf", attrib + i * nattrib + j);
+      safe_scanf(f, 1, "%lf", attrib + i * nattrib + j);
   }
   fclose(f);
   mesh_set_ents(m, elem_dim, nelems, verts_of_elems);
