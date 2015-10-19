dcb60408db8d599fb645a2fabb37877c5b2ed72e
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-19 12:02:27 -0400

    new term "linpart"
    definition: the cheap linear partitioning
    of a continuous range

--- test_global_part.c
+++ test_global_part.c
@@ -8,7 +8,7 @@ int main()
   unsigned long global[7] = {0,1,2,3,4,5,6};
   unsigned* part;
   unsigned* local;
-  global_to_parts(global, 7, 7, 4, &part, &local);
+  global_to_linpart(global, 7, 7, 4, &part, &local);
   for (unsigned i = 0; i < 7; ++i)
     printf("part %u local %u\n", part[i], local[i]);
   loop_free(part);
