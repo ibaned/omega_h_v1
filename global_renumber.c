fe10df099dfe65d05ee3bb0ef8f0c00880f29df6
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-26 16:03:19 -0500

    more drafting global_renumber

--- global_renumber.c
+++ global_renumber.c
@@ -34,17 +34,37 @@ unsigned long* global_renumber(
   unsigned* lin_idxs_recvd = LOOP_MALLOC(unsigned, nrecvd);
   comm_exch_uints(gc, 1, lin_idxs_sent, send_counts, send_offsets,
       lin_idxs_recvd, recv_counts, recv_offsets);
-  unsigned* src_parts_recvd = LOOP_MALLOC(unsigned, nrecvd);
+  unsigned* recv_of_recvd = LOOP_MALLOC(unsigned, nrecvd);
   for (unsigned i = 0; i < nrecvs; ++i) {
     unsigned first = recv_offsets[i];
     unsigned end = recv_offsets[i + 1];
     for (unsigned j = first; j < end; ++j)
-      src_parts_recvd[j] = recv_parts[i];
+      recv_of_recvd[j] = i;
   }
   unsigned linsize = linpart_size(total, nparts, comm_rank());
   unsigned* recvd_of_lins;
   unsigned* recvd_of_lin_offsets;
   invert_map(nrecvd, lin_idxs_recvd, linsize, &recvd_of_lins,
       &recvd_of_lin_offsets);
+  unsigned* recv_nents = LOOP_MALLOC(unsigned, nrecvs);
+  comm_sync_uint(gc, n, recv_nents);
+  unsigned* own_recv_of_recvd = LOOP_MALLOC(unsigned, nrecvd);
+  comm_sync_uint(gc, n, recv_nents);
+  for (unsigned i = 0; i < linsize; ++i) {
+    unsigned first = recvd_of_lin_offsets[i];
+    unsigned end = recvd_of_lin_offsets[i + 1];
+    unsigned owner_recv = recv_of_recvd[first];
+    for (unsigned j = first + 1; j < end; ++j) {
+      if (recv_nents[recv_of_recvd[j]] <
+          recv_nents[owner_recv])
+        owner_recv = recv_of_recvd[j];
+      else if ((recv_nents[recv_of_recvd[j]] ==
+                recv_nents[owner_recv]) &&
+               (recv_parts[recv_of_recvd[j]] <
+                recv_parts[owner_recv]))
+        owner_recv = recv_of_recvd[j];
+    }
+    own_recv_of_recvd[i] =  owner_recv;
+  }
   comm_free(gc);
 }
