cb4540a94eb0b4c63aa7c6b437b65ca975bd328c
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-27 17:27:19 -0500

    first parallel owners_from_global

--- test_global.c
+++ test_global.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "comm.h"
@@ -7,11 +8,19 @@
 int main()
 {
   comm_init();
-  unsigned n = 3;
-  unsigned long global[3] = {0,1,2};
   unsigned* own_parts;
   unsigned* own_idxs;
-  owners_from_global(n, global, &own_parts, &own_idxs);
+  unsigned n;
+  assert(comm_size() == 2);
+  if (comm_rank() == 0) {
+    n = 1;
+    unsigned long global[1] = {0};
+    owners_from_global(n, global, &own_parts, &own_idxs);
+  } else {
+    n = 1;
+    unsigned long global[1] = {0};
+    owners_from_global(n, global, &own_parts, &own_idxs);
+  }
   for (unsigned i = 0; i < n; ++i)
     printf("%u %u\n", own_parts[i], own_idxs[i]);
   loop_free(own_parts);
