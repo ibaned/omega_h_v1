1091763f07ed4622250cc6919b1eee50d719dea6
Benjamin Mizera <mizerb@rpi.edu>
2015-10-27 20:24:59 -0400

    Added loop exec funtion for invert maps

--- invert_map.c
+++ invert_map.c
@@ -4,15 +4,25 @@
 #include "loop.h"
 
 
-__global__ void Worker_Kernal( unsigned * in ,
+
+LOOP_KERNEL( Work , unsigned const* in ,
 		unsigned * offsets , unsigned * counts,
 		unsigned *out )
-{
-	int i = (blockIdx.x * blockDim.x + threadIdx.x);
-	out[ offsets[in[i]] + counts[in[i]]++  ] = i; //I mean....
+  unsigned d = in[i];
+  unsigned o = offsets[d];
+  unsigned j = counts[d];
+  out[o + j] = i;
+
+#ifdef __CUDACC__
+  atomicAdd( counts+d , 1);
+#elif defined(_OPENMP)
+  #pragma omp atomic
+    counts[d]++;
+#else
+  counts[d]++;
+#endif
+
 }
-
-
 
 
 
@@ -30,12 +40,13 @@ void invert_map(
   unsigned* offsets = uints_exscan(counts, nout);
   unsigned* out = LOOP_MALLOC(unsigned, nin);
   uints_zero(counts, nout);
-  for (unsigned i = 0; i < nin; ++i) {
+  LOOP_EXEC(Work, nin , in, offsets, counts , out);
+  /*  for (unsigned i = 0; i < nin; ++i) {
     unsigned d = in[i];
     unsigned o = offsets[d];
     unsigned j = counts[d]++;
     out[o + j] = i;
-  }
+  }*/
   loop_free(counts);
   *p_out = out;
   *p_offsets = offsets;
