b26cf8503de20c1dee0c9aeeedd9289f2ab37533
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-27 21:56:45 -0500

    update mpi and clang config examples

--- mpi_config.mk
+++ mpi_config.mk
@@ -1,4 +1,5 @@
 CC = mpicc
-CFLAGS = -g -O2 -std=c99 -Werror -Weverything -Wno-float-equal
-LDLIBS = -lm
+CPP = mpicc
+CPPFLAGS = -std=c99
+CFLAGS = -g -O2 -Weverything -Wno-float-equal -pedantic -Werror -Wno-format-nonliteral
 USE_MPI = 1
