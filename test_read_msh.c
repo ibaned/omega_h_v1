7329275f6e5bf6d42cfd635e1947eeda80170503
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-29 15:23:18 -0400

    test and debug gmsh reader

---
+++ test_read_msh.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+
+#include "gmsh_io.h"
+#include "mesh.h"
+#include "vtk.h"
+
+int main(int argc, char** argv)
+{
+  assert(argc == 3);
+  struct mesh* m = read_msh(argv[1]);
+  write_vtu(m, argv[2]);
+  free_mesh(m);
+}
