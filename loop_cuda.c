48ef7f1acf3383222e212e3770bc22e04f75a956
Benjamin Mizera <mizerb@rpi.edu>
2015-10-30 20:02:05 -0400

    Sucessful build w/ changes

--- loop_cuda.c
+++ loop_cuda.c
@@ -3,22 +3,11 @@
 
 #include "loop_cuda.h"
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 06912c4f5db3a5d28a87735d94a83187a07c9294
 void* loop_cuda_malloc(unsigned long n)
 {
   void* p;
   CUDACALL(cudaMalloc(&p, n));
   return p;
-}
-
-__device__  unsigned cuda_atomic_increment( unsigned * p )
-{
-	int a= *p;
-	atomicAdd( p , 1);
-	return a;
 }
 
 void loop_cuda_free(void* p)
@@ -38,4 +27,12 @@ void* loop_cuda_to_device(void const* p, unsigned long n)
   void* out = loop_cuda_malloc(n);
   CUDACALL(cudaMemcpy(out, p, n, cudaMemcpyHostToDevice));
   return out;
+}
+
+
+__device__  unsigned cuda_atomic_increment( unsigned * p )
+{
+	int a= *p;
+	atomicAdd( p , 1);
+	return a;
 }
