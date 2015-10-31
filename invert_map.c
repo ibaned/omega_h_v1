46605efef9fe1234046e16543c3407aed1b4d342
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-31 09:25:37 -0400

    cleanup comments

--- invert_map.c
+++ invert_map.c
@@ -22,17 +22,11 @@ void invert_map(
   unsigned* counts = LOOP_MALLOC(unsigned, nout);
   uints_zero(counts, nout);
   for (unsigned i = 0; i < nin; ++i)
-    counts[in[i]]++; //Broken Operation.
+    counts[in[i]]++; /* TODO: loop kernel for this */
   unsigned* offsets = uints_exscan(counts, nout);
   unsigned* out = LOOP_MALLOC(unsigned, nin);
   uints_zero(counts, nout);
   LOOP_EXEC(Work, nin, in, offsets, counts, out);
-  /*  for (unsigned i = 0; i < nin; ++i) {
-    unsigned d = in[i];
-    unsigned o = offsets[d];
-    unsigned j = counts[d]++;
-    out[o + j] = i;
-  }*/
   loop_free(counts);
   *p_out = out;
   *p_offsets = offsets;
