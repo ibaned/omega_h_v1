21d77a94059a8f4c95d58cd96d62003795ee44d8
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-11-08 16:54:01 -0500

    more renaming part to rank

--- owners_from_global.c
+++ owners_from_global.c
@@ -10,17 +10,17 @@
 void owners_from_global(
     unsigned n,
     unsigned long* global_in,
-    unsigned** p_own_parts,
+    unsigned** p_own_ranks,
     unsigned** p_own_idxs)
 {
   unsigned long total = comm_max_ulong(ulongs_max(global_in, n)) + 1;
-  unsigned nparts = comm_size();
-  unsigned* lin_parts;
+  unsigned nranks = comm_size();
+  unsigned* lin_ranks;
   unsigned* lin_idxs;
-  global_to_linpart(global_in, n, total, nparts, &lin_parts, &lin_idxs);
-  unsigned linsize = linpart_size(total, nparts, comm_rank());
-  struct exchanger* ex = new_exchanger(n, linsize, lin_parts, lin_idxs);
-  loop_free(lin_parts);
+  global_to_linpart(global_in, n, total, nranks, &lin_ranks, &lin_idxs);
+  unsigned linsize = linpart_size(total, nranks, comm_rank());
+  struct exchanger* ex = new_exchanger(n, linsize, lin_ranks, lin_idxs);
+  loop_free(lin_ranks);
   loop_free(lin_idxs);
   unsigned const* recv_of_recvd = ex->recv_of_recvd;
   unsigned const* recvd_of_lins = ex->recvd_of_dests;
@@ -30,7 +30,7 @@ void owners_from_global(
     orig_idxs[i] = i;
   unsigned* orig_idxs_recvd = exchange_uints(ex, 1, orig_idxs);
   loop_free(orig_idxs);
-  unsigned* own_part_of_recvd = LOOP_MALLOC(unsigned, ex->nrecvd);
+  unsigned* own_rank_of_recvd = LOOP_MALLOC(unsigned, ex->nrecvd);
   unsigned* own_idx_of_recvd = LOOP_MALLOC(unsigned, ex->nrecvd);
   unsigned* recv_nents = LOOP_MALLOC(unsigned, ex->nrecvs);
   comm_sync_uint(ex->forward_comm, n, recv_nents);
@@ -56,15 +56,15 @@ void owners_from_global(
     }
     for (unsigned j = first; j < end; ++j) {
       recvd = recvd_of_lins[j];
-      own_part_of_recvd[recvd] = ex->recv_ranks[own_recv];
+      own_rank_of_recvd[recvd] = ex->recv_ranks[own_recv];
       own_idx_of_recvd[recvd] = own_idx;
     }
   }
   loop_free(orig_idxs_recvd);
   loop_free(recv_nents);
-  *p_own_parts = unexchange_uints(ex, 1, own_part_of_recvd);
+  *p_own_ranks = unexchange_uints(ex, 1, own_rank_of_recvd);
   *p_own_idxs = unexchange_uints(ex, 1, own_idx_of_recvd);
-  loop_free(own_part_of_recvd);
+  loop_free(own_rank_of_recvd);
   loop_free(own_idx_of_recvd);
   free_exchanger(ex);
 }
