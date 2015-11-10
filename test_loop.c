03022b01df09422b29da26a6feb1217b0d939944
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-11-10 13:23:24 -0500

    clean up loop test

--- test_loop.c
+++ test_loop.c
@@ -9,106 +9,88 @@
 
 static void Ints_Test()
 {
-  unsigned *a, *b, zz; clock_t start , end;
-#ifdef __CUDACC__
-  unsigned *c;
-  unsigned *d;
-#endif
+  unsigned* a;
+  unsigned* b;
+  unsigned* c;
+  unsigned* d;
+  unsigned zz;
+  clock_t start;
+  clock_t end;
+
   //Zero (Runtime)
-  a = LOOP_MALLOC(unsigned , N);
+  a = LOOP_MALLOC(unsigned, N);
   start = clock();
-  uints_zero( a , N);
+  uints_zero(a, N);
   end = clock();
-
-#ifdef __CUDACC__
-  b = (unsigned *)loop_cuda_to_host( a , N * sizeof(unsigned));
-  for( unsigned i = 0 ; i < N ; i++)
-    if( b[i] != 0){ printf("zero failed"); abort();}
-  free(b);
-#else
-  for( unsigned i = 0 ; i < N ; i++)
-	  if( a[i] != 0){ printf("zero failed"); abort();}
-#endif
-
-  loop_free(a);;
-  printf( "ZERO: %F\n" , (double)(end-start)/CLOCKS_PER_SEC);
-
+  b = (unsigned*) loop_to_host(a, N * sizeof(unsigned));
+  for(unsigned i = 0; i < N; ++i)
+    if(b[i] != 0) {
+      printf("zero failed");
+      abort();
+    }
+  loop_host_free(b);
+  loop_free(a);
+  printf("ZERO: %F\n", (double)(end-start)/CLOCKS_PER_SEC);
 
   //units_copy (Max values ?? ; Min values ?? ; Runtime)
-  a = (unsigned *)malloc(N *sizeof(unsigned));
-  for( int i =0 ; i< N ;i++)
+  a = LOOP_HOST_MALLOC(unsigned, N);
+  for(unsigned i = 0; i < N ;i++)
   {
 	  a[i] = ((unsigned)rand()) % 0xFFFFFFFF;
   }
-#ifdef __CUDACC__
-  c = (unsigned *)loop_cuda_to_device( a , N* sizeof(unsigned));
+  c = (unsigned*) loop_to_device(a, N * sizeof(unsigned));
   start = clock();
-  d = uints_copy ( c , N);
+  d = uints_copy(c, N);
   end = clock();
-  b = (unsigned *)loop_cuda_to_host( d , N*sizeof(unsigned));
-
-  loop_free(c); loop_free(d);
-#else
-  start = clock();
-  b = uints_copy( a , N);
-  end = clock();
-#endif
-
-  for(unsigned i = 0; i<N;i++)
-    if( a[i] != b[i]){ printf("uints copy failed!"); abort();}
-  free(b); free(a);
+  b = (unsigned*) loop_to_host(d, N * sizeof(unsigned));
+  loop_free(c);
+  loop_free(d);
+  for(unsigned i = 0; i < N; ++i)
+    if(a[i] != b[i]) {
+      printf("uints copy failed!");
+      abort();
+    }
+  loop_host_free(b);
+  loop_host_free(a);
   printf( "COPY: %F\n" , (double)(end-start)/CLOCKS_PER_SEC);
 
-
   //units_max (Max values; Min values; Runtime)
-
-  a = (unsigned *)malloc(N *sizeof(unsigned));
-  for( int i =0 ; i< N ;i++)
+  a = (unsigned*) malloc(N * sizeof(unsigned));
+  for(unsigned i = 0; i < N; ++i)
   {
 	  a[i] = ((unsigned)rand()) % 0xFFFFFFFF;
   }
   a[0] = 0xFFFFFFFF; //Max value
-
-#ifdef __CUDACC__
-  c = (unsigned *)loop_cuda_to_device( a , N* sizeof(unsigned));
+  c = (unsigned*) loop_to_device(a, N * sizeof(unsigned));
   start = clock();
-  zz = uints_max ( c , N);
+  zz = uints_max(c, N);
   end = clock();
   loop_free(c);
-#else
-  start = clock();
-  zz = uints_max( a , N);
-  end = clock();
-#endif
-  free(a);
-
-  if ( zz != 0xFFFFFFFF){ printf( "MAX FAILED"); abort();}
-
+  loop_host_free(a);
+  if (zz != 0xFFFFFFFF) {
+    printf( "MAX FAILED");
+    abort();
+  }
   printf( "MAX: %F\n" , (double)(end-start)/CLOCKS_PER_SEC);
 
   // uints_exscan (Max values; Min values; Runtime)
-  a = (unsigned *)malloc(N *sizeof(unsigned));
+  a = LOOP_HOST_MALLOC(unsigned, N);
   for( int i =0 ; i< N ;i++)
   {
 	  a[i] = ((unsigned)rand()) % 0xFFFFFFFF;
   }
-
+  /* todo ? */
+  loop_host_free(a);
 
   // uints_unscan (Max values; Min values; Runtime)
 
-
-
-
   // uints_negate (I'm confused)
-
-
-
 }
 
-LOOP_KERNEL(count ,
-	unsigned const* in ,
-	unsigned * counts)
-	loop_atomic_increment(&(counts[in[i]]));
+LOOP_KERNEL(count,
+    unsigned const* in,
+    unsigned* counts)
+  loop_atomic_increment(&(counts[in[i]]));
 }
 
 static void COUNT_TEST()
@@ -119,7 +101,8 @@ static void COUNT_TEST()
   unsigned* counts = LOOP_MALLOC(unsigned, 7);
   uints_zero(counts, 7);
 
-  LOOP_EXEC( count , 7 , b , counts);
+  LOOP_EXEC(count, 7, b, counts);
+  loop_free(b);
 
   unsigned* z = (unsigned *)loop_to_host(counts, sizeof(unsigned) * 7);
 
@@ -128,6 +111,7 @@ static void COUNT_TEST()
     printf("%d\n", z[i]);
   }
   loop_free(counts);
+  loop_host_free(z);
 }
 
 
@@ -137,21 +121,23 @@ int main()
   cudaDeviceReset();
 #endif
 
-  unsigned  a[7] = {1,2,1,3,5,1,2};
-  unsigned *b  = (unsigned *)loop_to_device(a , sizeof(unsigned)*7 );
+  unsigned a[7] = {1,2,1,3,5,1,2};
+  unsigned* b = (unsigned*) loop_to_device(a, sizeof(unsigned) * 7);
 
   unsigned* counts = LOOP_MALLOC(unsigned, 7);
   uints_zero(counts, 7);
 
-  LOOP_EXEC( count , 7 , b , counts);
+  LOOP_EXEC(count, 7, b, counts);
+  loop_free(b);
 
-  unsigned *z = (unsigned *)loop_to_host( counts, sizeof(unsigned)*7);
+  unsigned* z = (unsigned*) loop_to_host(counts, sizeof(unsigned) * 7);
 
-  for(int i = 0 ; i<7; i++)
+  for(unsigned i = 0; i < 7; ++i)
   {
-	  printf( "%d\n" ,z[i]);
+	  printf("%d\n", z[i]);
   }
-  loop_free( counts);
+  loop_free(counts);
+  loop_host_free(z);
 
   Ints_Test();
   COUNT_TEST();
