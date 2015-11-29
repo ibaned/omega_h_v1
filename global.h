cf373a579da6b7504eb0439e997dc3e3aed0c9c9
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-11-29 18:14:13 -0500

    don't need category sort/unsort anymore

--- global.h
+++ global.h
@@ -10,44 +10,4 @@ void global_to_linpart(unsigned long const* global, unsigned n,
     unsigned** p_part, unsigned** p_local);
 unsigned linpart_size(unsigned long total, unsigned nparts, unsigned part);
 
-unsigned* sort_uints_by_category(
-    unsigned const* a,
-    unsigned width,
-    unsigned n,
-    unsigned const* cats,
-    unsigned const* indices,
-    unsigned const* cat_offsets);
-
-double* sort_doubles_by_category(
-    double const* a,
-    unsigned width,
-    unsigned n,
-    unsigned const* cats,
-    unsigned const* indices,
-    unsigned const* cat_offsets);
-
-unsigned* unsort_uints_by_category(
-    unsigned const* a,
-    unsigned width,
-    unsigned n,
-    unsigned const* cats,
-    unsigned const* indices,
-    unsigned const* cat_offsets);
-
-double* unsort_doubles_by_category(
-    double const* a,
-    unsigned width,
-    unsigned n,
-    unsigned const* cats,
-    unsigned const* indices,
-    unsigned const* cat_offsets);
-
-unsigned long* unsort_ulongs_by_category(
-    unsigned long const* a,
-    unsigned width,
-    unsigned n,
-    unsigned const* cats,
-    unsigned const* indices,
-    unsigned const* cat_offsets);
-
 #endif
