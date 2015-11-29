f78b762a70345f992d7fa5437a308e6d73b6f1e8
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-11-29 09:39:34 -0500

    RIB test overhaul starting with cloud

--- test_rib.c
+++ test_rib.c
@@ -1,50 +1,31 @@
 #include <assert.h>
-#include <stdio.h>
+#include <stdlib.h>
 
+#include "cloud.h"
 #include "comm.h"
 #include "loop.h"
-#include "parallel_inertial_bisect.h"
+#include "vtk.h"
 
-int main()
+static struct cloud* make_shuffled_cloud(unsigned n)
+{
+  struct cloud* c = new_cloud(n);
+  double* coords = LOOP_MALLOC(double, 3 * n);
+  for (unsigned i = 0; i < n; ++i) {
+    coords[i * 3 + 0] = (double) i + (1.0 / comm_size()) * comm_rank();
+    coords[i * 3 + 1] = 0;
+    coords[i * 3 + 2] = 0;
+  }
+  cloud_add_tag(c, TAG_F64, "coordinates", 3, coords);
+  return c;
+}
+
+int main(int argc, char** argv)
 {
   comm_init();
-  assert(comm_size() == 2);
-  unsigned const n_in[2] = {2,2};
-  double const coords_in[2][6] = {
-    {0,0,0, 2,0,0},
-    {1,0,0, 3,0,0}};
-  double const masses_in[2][2] = {
-    {1, 1},
-    {1, 1}};
-  unsigned const orig_ranks_in[2][2] = {
-    {0, 0},
-    {1, 1}};
-  unsigned const orig_ids_in[2][2] = {
-    {0, 1},
-    {0, 1}};
-  unsigned n = n_in[comm_rank()];
-  double* coords = loop_to_device(coords_in[comm_rank()],
-      sizeof(double) * 3 * n);
-  double* masses = loop_to_device(masses_in[comm_rank()],
-      sizeof(double) * 3 * n);
-  unsigned* orig_ranks = loop_to_device(orig_ranks_in[comm_rank()],
-      sizeof(unsigned) * n);
-  unsigned* orig_ids = loop_to_device(orig_ids_in[comm_rank()],
-      sizeof(unsigned) * n);
-  recursive_inertial_bisect(&n, &coords, &masses, &orig_ranks, &orig_ids);
-  printf("n %u\n", n);
-  for (unsigned i = 0; i < n; ++i)
-    printf("%u %u : (%f %f %f) %f %u %u\n",
-        comm_rank(), i,
-        coords[i * 3 + 0],
-        coords[i * 3 + 1],
-        coords[i * 3 + 2],
-        masses[i],
-        orig_ranks[i],
-        orig_ids[i]);
-  loop_free(coords);
-  loop_free(masses);
-  loop_free(orig_ranks);
-  loop_free(orig_ids);
+  assert(argc == 2);
+  unsigned n = (unsigned) atoi(argv[1]);
+  struct cloud* c = make_shuffled_cloud(n);
+  write_parallel_vtu_cloud(c, "before.pvtu");
+  free_cloud(c);
   comm_fini();
 }
