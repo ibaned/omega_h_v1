f44e92e82a2e8605912f45f8c720b313b015dfc1
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-02 15:45:42 -0500

    start really testing migration

--- test_migrate.c
+++ test_migrate.c
@@ -18,18 +18,18 @@ int main()
     m = new_empty_mesh(1);
   mesh_number_simply(m);
   write_parallel_vtu(m, "before.pvtu");
-//if (comm_rank() == 0) {
-//  unsigned n = 0;
-//  unsigned recvd_elem_ranks[1];
-//  unsigned recvd_elem_ids[1];
-//  migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
-//} else {
-//  unsigned n = 1;
-//  unsigned recvd_elem_ranks[1] = {0};
-//  unsigned recvd_elem_ids[1] = {0};
-//  migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
-//}
-//write_parallel_vtu(m, "after.pvtu");
+  if (comm_rank() == 0) {
+    unsigned n = 0;
+    unsigned recvd_elem_ranks[1];
+    unsigned recvd_elem_ids[1];
+    migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
+  } else {
+    unsigned n = 1;
+    unsigned recvd_elem_ranks[1] = {0};
+    unsigned recvd_elem_ids[1] = {0};
+    migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
+  }
+  write_parallel_vtu(m, "after.pvtu");
   free_mesh(m);
   comm_fini();
 }
