5a358d4a93308becc91eacad16e6cdf17de2080e
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-02 18:24:02 -0500

    raise migration test to 2D

--- test_migrate.c
+++ test_migrate.c
@@ -13,20 +13,20 @@ int main()
   struct mesh* m = 0;
   assert(comm_size() == 2);
   if (comm_rank() == 0)
-    m = new_box_mesh(1);
+    m = new_box_mesh(2);
   else
-    m = new_empty_mesh(1);
+    m = new_empty_mesh(2);
   mesh_number_simply(m);
   write_parallel_vtu(m, "before.pvtu");
   if (comm_rank() == 0) {
-    unsigned n = 0;
-    unsigned recvd_elem_ranks[1];
-    unsigned recvd_elem_ids[1];
+    unsigned n = 1;
+    unsigned recvd_elem_ranks[1] = {0};
+    unsigned recvd_elem_ids[1] = {0};
     migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
   } else {
     unsigned n = 1;
     unsigned recvd_elem_ranks[1] = {0};
-    unsigned recvd_elem_ids[1] = {0};
+    unsigned recvd_elem_ids[1] = {1};
     migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
   }
   write_parallel_vtu(m, "after.pvtu");
