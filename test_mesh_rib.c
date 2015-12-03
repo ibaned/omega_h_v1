3dc334ab54737886208b1f366c6c1d7424e50bde
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-03 16:42:04 -0500

    bigger mesh and re-number in RIB test

--- test_mesh_rib.c
+++ test_mesh_rib.c
@@ -2,6 +2,7 @@
 #include "global.h"
 #include "mesh.h"
 #include "parallel_inertial_bisect.h"
+#include "parallel_mesh.h"
 #include "refine_by_size.h"
 #include "vtk.h"
 
@@ -11,13 +12,14 @@ int main()
   struct mesh* m = 0;
   if (comm_rank() == 0) {
     m = new_box_mesh(2);
-    for (unsigned i = 0; i < 1; ++i)
+    for (unsigned i = 0; i < 5; ++i)
       uniformly_refine(&m);
   } else
     m = new_empty_mesh(2);
   mesh_number_simply(m);
   write_parallel_vtu(m, "before.pvtu");
   balance_mesh_inertial(&m);
+  mesh_global_renumber(m, 0);
   write_parallel_vtu(m, "after.pvtu");
   free_mesh(m);
   comm_fini();
