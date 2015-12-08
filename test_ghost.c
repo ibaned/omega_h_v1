b2a2c7ecae9ab927acdef6c7b65e396013f8cdc9
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-07 19:39:43 -0500

    2 layer ghosting works on 1D test case

--- test_ghost.c
+++ test_ghost.c
@@ -15,7 +15,7 @@ int main()
   struct mesh* m = 0;
   if (comm_rank() == 0) {
     m = new_box_mesh(1);
-    for (unsigned i = 0; i < 2; ++i)
+    for (unsigned i = 0; i < 3; ++i)
       uniformly_refine(&m);
   }
   m = bcast_mesh_metadata(m);
@@ -23,7 +23,7 @@ int main()
   balance_mesh_inertial(&m);
   mesh_global_renumber(m, 0);
   write_parallel_vtu(m, "before.pvtu");
-  ghost_mesh(&m, 1);
+  ghost_mesh(&m, 2);
   write_parallel_vtu(m, "after.pvtu");
   free_mesh(m);
   comm_fini();
