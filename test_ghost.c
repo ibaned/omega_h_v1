692fb45e86555e1a9527749d74fd55d118bd47fb
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-07 20:07:08 -0500

    triangle mesh ghosting also works

--- test_ghost.c
+++ test_ghost.c
@@ -14,8 +14,8 @@ int main()
   comm_init();
   struct mesh* m = 0;
   if (comm_rank() == 0) {
-    m = new_box_mesh(1);
-    for (unsigned i = 0; i < 2; ++i)
+    m = new_box_mesh(2);
+    for (unsigned i = 0; i < 4; ++i)
       uniformly_refine(&m);
   }
   m = bcast_mesh_metadata(m);
@@ -23,7 +23,7 @@ int main()
   balance_mesh_inertial(&m);
   mesh_global_renumber(m, 0);
   write_parallel_vtu(m, "before.pvtu");
-  ghost_mesh(&m, 2);
+  ghost_mesh(&m, 1);
   write_parallel_vtu(m, "after.pvtu");
   free_mesh(m);
   comm_fini();
