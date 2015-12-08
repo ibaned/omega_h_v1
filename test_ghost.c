10511feb940634378d0820996a7c255c095bd1c2
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-08 09:09:02 -0500

    refactor test_ghost.c into ghosting utility

--- test_ghost.c
+++ test_ghost.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 
 #include "bcast.h"
 #include "comm.h"
@@ -9,22 +10,21 @@
 #include "refine_by_size.h"
 #include "vtk.h"
 
-int main()
+int main(int argc, char** argv)
 {
   comm_init();
+  assert(argc == 4);
   struct mesh* m = 0;
-  if (comm_rank() == 0) {
-    m = new_box_mesh(2);
-    for (unsigned i = 0; i < 4; ++i)
-      uniformly_refine(&m);
-  }
+  if (comm_rank() == 0)
+    m = read_vtu(argv[1]);
   m = bcast_mesh_metadata(m);
   mesh_number_simply(m, 0);
   balance_mesh_inertial(&m);
   mesh_global_renumber(m, 0);
-  write_parallel_vtu(m, "before.pvtu");
-  ghost_mesh(&m, 1);
-  write_parallel_vtu(m, "after.pvtu");
+  unsigned nlayers = (unsigned) atoi(argv[2]);
+  assert(nlayers <= 10);
+  ghost_mesh(&m, nlayers);
+  write_parallel_vtu(m, argv[3]);
   free_mesh(m);
   comm_fini();
 }
