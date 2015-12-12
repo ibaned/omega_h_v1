9ded17b740a93e833018ce48452718ebf74e0ccc
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-12 16:55:24 -0500

    make osh_new_field idempotent

--- omega_h.c
+++ omega_h.c
@@ -72,6 +72,8 @@ void osh_set_field(osh_t m, char const* name, unsigned ncomps, double* data)
 
 void osh_new_field(osh_t m, char const* name, unsigned ncomps)
 {
+  if (mesh_find_tag((struct mesh*)m, 0, name))
+    return;
   double* data = LOOP_MALLOC(double, ncomps * mesh_count((struct mesh*)m, 0));
   mesh_add_tag((struct mesh*)m, 0, TAG_F64, name, ncomps, data);
 }
