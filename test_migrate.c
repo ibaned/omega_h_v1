9c1a2f3fdc9172544e891a5bd1e34b74e9510309
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-18 11:59:40 -0500

    test edge migration, fix free_exchanger calls

--- test_migrate.c
+++ test_migrate.c
@@ -2,10 +2,12 @@
 
 #include "bcast.h"
 #include "comm.h"
+#include "ints.h"
 #include "loop.h"
 #include "mesh.h"
 #include "migrate_mesh.h"
 #include "parallel_mesh.h"
+#include "subset.h"
 #include "vtk.h"
 
 int main()
@@ -13,10 +15,13 @@ int main()
   comm_init();
   assert(comm_size() == 2);
   struct mesh* m = 0;
-  if (comm_rank() == 0)
+  if (comm_rank() == 0) {
     m = new_box_mesh(2);
+    mesh_count(m, 1); //trigger edge creation
+  }
   m = bcast_mesh_metadata(m);
   mesh_number_simply(m, 0);
+  mesh_number_simply(m, 1);
   write_parallel_vtu(m, "before.pvtu");
   if (comm_rank() == 0) {
     unsigned n = 1;
@@ -29,7 +34,11 @@ int main()
     unsigned recvd_elem_ids[2] = {1,0};
     migrate_mesh(&m, n, recvd_elem_ranks, recvd_elem_ids);
   }
-  write_parallel_vtu(m, "after.pvtu");
+  unsigned* offsets = uints_linear(mesh_count(m, 1) + 1, 1);
+  struct mesh* sm = subset_mesh(m, 1, offsets);
+  loop_free(offsets);
   free_mesh(m);
+  write_parallel_vtu(sm, "after.pvtu");
+  free_mesh(sm);
   comm_fini();
 }
