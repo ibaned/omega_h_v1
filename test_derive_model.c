217b69690e21401e63c85141d6be5059dbbabc70
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-22 19:32:51 -0500

    derive_model.exe gets input/output files

--- test_derive_model.c
+++ test_derive_model.c
@@ -2,28 +2,15 @@
 #include <stdlib.h>
 
 #include "algebra.h"
-#include "ints.h"
-#include "loop.h"
 #include "derive_model.h"
 #include "mesh.h"
-#include "subset.h"
 #include "vtk.h"
 
 int main(int argc, char** argv)
 {
-  assert(argc == 3);
-  unsigned mesh_dim = (unsigned) atoi(argv[1]);
-  assert(1 <= mesh_dim);
-  assert(mesh_dim <= 3);
-  unsigned out_dim = (unsigned) atoi(argv[2]);
-  assert(1 <= out_dim);
-  assert(out_dim <= 3);
-  struct mesh* m = new_box_mesh(mesh_dim);
-  mesh_derive_model(m, PI / 4);
-  unsigned* offsets = uints_linear(mesh_count(m, out_dim) + 1, 1);
-  struct mesh* sm = subset_mesh(m, out_dim, offsets);
-  loop_free(offsets);
+  assert(argc == 4);
+  struct mesh* m = read_vtu(argv[1]);
+  mesh_derive_model(m, PI / atof(argv[2]));
+  write_vtu(m, argv[3]);
   free_mesh(m);
-  write_vtu(sm, "out.vtu");
-  free_mesh(sm);
 }
