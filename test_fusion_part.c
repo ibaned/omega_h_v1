1203f1a907158e0831f682ffb20baa7d50ed2df3
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-23 18:37:18 -0500

    tuning fusion partitioner

--- test_fusion_part.c
+++ test_fusion_part.c
@@ -15,13 +15,16 @@ int main()
     m = read_vtu("xgc.vtu");
   m = bcast_mesh_metadata(m);
   mesh_number_simply(m, 0);
+  mesh_number_simply(m, 2);
   mesh_interp_to_elems(m, "coordinates");
   double* elem_coords = mesh_find_tag(m, 2, "coordinates")->d.f64;
   unsigned nelems = mesh_count(m, 2);
-  double const center[2] = {1.75, 0};
+  double const center[2] = {1.71, 0};
+  double aspect = 1.5;
   for (unsigned i = 0; i < nelems; ++i) {
     double rel[2];
     subtract_vectors(elem_coords + i * 3, center, rel, 2);
+    rel[1] /= aspect;
     elem_coords[i * 3 + 0] = vector_norm(rel, 2);
     elem_coords[i * 3 + 1] = 0;
     elem_coords[i * 3 + 2] = 0;
