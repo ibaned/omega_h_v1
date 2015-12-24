aa19e72f0220a13f0cc5e47c132696b14cbc3347
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-24 14:48:15 -0500

    factor out element refinement code

--- refine_common.c
+++ refine_common.c
@@ -39,6 +39,44 @@ static void refine_verts(struct mesh* m, struct mesh* m_out,
   refine_class(m, m_out, src_dim, gen_offset_of_srcs);
 }
 
+static void refine_elems(struct mesh* m, struct mesh* m_out,
+    unsigned src_dim, unsigned const* gen_offset_of_srcs)
+{
+  unsigned elem_dim = mesh_dim(m);
+  unsigned nsrcs = mesh_count(m, src_dim);
+  unsigned nverts = mesh_count(m, 0);
+  unsigned* gen_vert_of_srcs = LOOP_MALLOC(unsigned, nsrcs);
+  for (unsigned i = 0; i < nsrcs; ++i)
+    if (gen_offset_of_srcs[i] != gen_offset_of_srcs[i + 1])
+      gen_vert_of_srcs[i] = nverts + gen_offset_of_srcs[i];
+  unsigned* gen_offset_of_elems;
+  unsigned* gen_direction_of_elems;
+  unsigned* gen_vert_of_elems;
+  mesh_splits_to_elements(m, src_dim, gen_offset_of_srcs, gen_vert_of_srcs,
+      &gen_offset_of_elems, &gen_direction_of_elems, &gen_vert_of_elems);
+  loop_free(gen_vert_of_srcs);
+  unsigned ngen_elems;
+  unsigned* verts_of_gen_elems;
+  mesh_refine_topology(m, src_dim, elem_dim,
+      gen_offset_of_elems, gen_vert_of_elems, gen_direction_of_elems,
+      &ngen_elems, &verts_of_gen_elems);
+  loop_free(gen_vert_of_elems);
+  loop_free(gen_direction_of_elems);
+  unsigned nelems = mesh_count(m, elem_dim);
+  unsigned* offset_of_same_elems = uints_negate_offsets(
+      gen_offset_of_elems, nelems);
+  loop_free(gen_offset_of_elems);
+  unsigned const* verts_of_elems = mesh_ask_down(m, elem_dim, 0);
+  unsigned nelems_out;
+  unsigned* verts_of_elems_out;
+  concat_verts_of_ents(elem_dim, nelems, ngen_elems, verts_of_elems,
+      offset_of_same_elems, verts_of_gen_elems,
+      &nelems_out, &verts_of_elems_out);
+  loop_free(offset_of_same_elems);
+  loop_free(verts_of_gen_elems);
+  mesh_set_ents(m_out, elem_dim, nelems_out, verts_of_elems_out);
+}
+
 unsigned refine_common(
     struct mesh** p_m,
     unsigned src_dim,
@@ -60,40 +98,10 @@ unsigned refine_common(
   unsigned* gen_offset_of_srcs = mesh_indset_offsets(m, src_dim, candidates,
       src_quals);
   loop_free(src_quals);
-  unsigned nverts = mesh_count(m, 0);
-  unsigned* gen_vert_of_srcs = LOOP_MALLOC(unsigned, nsrcs);
-  for (unsigned i = 0; i < nsrcs; ++i)
-    if (gen_offset_of_srcs[i] != gen_offset_of_srcs[i + 1])
-      gen_vert_of_srcs[i] = nverts + gen_offset_of_srcs[i];
   struct mesh* m_out = new_mesh(elem_dim);
   refine_verts(m, m_out, src_dim, gen_offset_of_srcs);
-  unsigned* gen_offset_of_elems;
-  unsigned* gen_direction_of_elems;
-  unsigned* gen_vert_of_elems;
-  mesh_splits_to_elements(m, src_dim, gen_offset_of_srcs, gen_vert_of_srcs,
-      &gen_offset_of_elems, &gen_direction_of_elems, &gen_vert_of_elems);
-  loop_free(gen_vert_of_srcs);
+  refine_elems(m, m_out, src_dim, gen_offset_of_srcs);
   loop_free(gen_offset_of_srcs);
-  unsigned ngen_elems;
-  unsigned* verts_of_gen_elems;
-  mesh_refine_topology(m, src_dim, elem_dim,
-      gen_offset_of_elems, gen_vert_of_elems, gen_direction_of_elems,
-      &ngen_elems, &verts_of_gen_elems);
-  loop_free(gen_vert_of_elems);
-  loop_free(gen_direction_of_elems);
-  unsigned nelems = mesh_count(m, elem_dim);
-  unsigned* offset_of_same_elems = uints_negate_offsets(
-      gen_offset_of_elems, nelems);
-  loop_free(gen_offset_of_elems);
-  unsigned const* verts_of_elems = mesh_ask_down(m, elem_dim, 0);
-  unsigned nelems_out;
-  unsigned* verts_of_elems_out;
-  concat_verts_of_ents(elem_dim, nelems, ngen_elems, verts_of_elems,
-      offset_of_same_elems, verts_of_gen_elems,
-      &nelems_out, &verts_of_elems_out);
-  mesh_set_ents(m_out, elem_dim, nelems_out, verts_of_elems_out);
-  loop_free(offset_of_same_elems);
-  loop_free(verts_of_gen_elems);
   free_mesh(m);
   *p_m = m_out;
   return 1;
