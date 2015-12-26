58212f67851eb8417d0fbeaa62b7f959431fe6e8
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-26 12:15:25 -0500

    full rep refinement improving...
    the vertices along boundary edges
    aren't classified right though...

--- test_refine_by_size.c
+++ test_refine_by_size.c
@@ -23,7 +23,7 @@ static void size_fun(double const* x, double* s)
 
 int main()
 {
-  struct mesh* m = new_box_mesh(3);
+  struct mesh* m = new_box_mesh(2);
   mesh_derive_model(m, PI / 4);
   mesh_set_rep(m, MESH_FULL);
   char fname[64];
