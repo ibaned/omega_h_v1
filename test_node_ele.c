8c0feee59bfb83ba490e58593a405927f580b3f7
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-12-26 11:26:03 -0500

    free memory in node_ele.exe

--- test_node_ele.c
+++ test_node_ele.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 
 #include "node_ele_io.h"
+#include "mesh.h"
 #include "vtk.h"
 
 int main(int argc, char** argv)
@@ -9,4 +10,5 @@ int main(int argc, char** argv)
   struct mesh* m = read_dot_node(argv[1]);
   read_dot_ele(m, argv[2]);
   write_vtu(m, argv[3]);
+  free_mesh(m);
 }
