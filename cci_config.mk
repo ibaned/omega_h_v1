e4bc9ba2ff0340775b377451cafe44de65557221
Dan Ibanez <ibaned@rpi.edu>
2015-12-28 16:57:07 -0500

    config without MPI3 works on CCI BG/Q

--- cci_config.mk
+++ cci_config.mk
@@ -2,7 +2,8 @@
 CC = mpicc
 CPP = mpicc
 CFLAGS = -O2 -Wall -Wextra -pedantic -std=c99
-LDFLAGS = -flto -O2
+LDFLAGS = -O2
 LDLIBS = -lm
+USE_MPI = 1
 # CCI's MPI library is older than MPI3.
-USE_MPI = 0
+USE_MPI3 = 0
