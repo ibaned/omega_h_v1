303a46d4646de34a070ac1a9c3565cde8ab582b4
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-10 18:40:14 -0500

    fix node_ele.exe

--- test_node_ele.c
+++ test_node_ele.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 
+#include "comm.h"
 #include "node_ele_io.h"
 #include "mesh.h"
 #include "vtk.h"
@@ -7,8 +8,10 @@
 int main(int argc, char** argv)
 {
   assert(argc == 4);
+  comm_init();
   struct mesh* m = read_dot_node(argv[1]);
   read_dot_ele(m, argv[2]);
   write_mesh_vtk(m, argv[3]);
   free_mesh(m);
+  comm_fini();
 }
