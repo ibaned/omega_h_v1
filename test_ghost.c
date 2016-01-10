6118540dc8d660a96f1ea98b4dedfe46caa9df52
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-10 16:04:58 -0500

    fix some parallel regressions

--- test_ghost.c
+++ test_ghost.c
@@ -15,11 +15,10 @@ int main(int argc, char** argv)
   comm_init();
   assert(argc == 4);
   struct mesh* m = 0;
-  if (comm_rank() == 0) {
+  if (comm_rank() == 0)
     m = read_vtu(argv[1]);
-    mesh_make_parallel(m);
-  }
   m = bcast_mesh_metadata(m);
+  mesh_make_parallel(m);
   balance_mesh_inertial(&m);
   mesh_global_renumber(m, 0);
   unsigned nlayers = (unsigned) atoi(argv[2]);
