670eff48f1ce6d28046fdb978742ff15d2e737ef
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-17 08:44:41 -0500

    draft faster dual when sides exist

--- dual.h
+++ dual.h
@@ -13,8 +13,17 @@ unsigned* dual_from_verts(
     unsigned const* elems_of_verts_offsets,
     unsigned const* elems_of_verts);
 
+unsigned* dual_from_sides(
+    unsigned elem_dim,
+    unsigned nelems,
+    unsigned const* sides_of_elems,
+    unsigned const* elems_of_sides_offsets,
+    unsigned const* elems_of_sides);
+
 struct mesh;
 
 unsigned* mesh_get_dual_from_verts(struct mesh* m);
+
+unsigned* mesh_get_dual_from_sides(struct mesh* m);
 
 #endif
