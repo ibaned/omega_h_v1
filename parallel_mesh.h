6f987fc0a609cfe3a094869e10689f06d72e3258
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-19 13:55:44 -0500

    GENERIC_MESH_CONFORM
    just like mesh_conform_tag but
    acts on arrays not tagged to the mesh.

--- parallel_mesh.h
+++ parallel_mesh.h
@@ -15,6 +15,9 @@ struct exchanger* mesh_ask_exchanger(struct mesh* m, unsigned dim);
 
 void mesh_global_renumber(struct mesh* m, unsigned dim);
 
+void mesh_conform_doubles(struct mesh* m, unsigned dim, unsigned width,
+    double** a);
+
 void mesh_conform_tag(struct mesh* m, unsigned dim, const char* name);
 void mesh_accumulate_tag(struct mesh* m, unsigned dim, const char* name);
 
