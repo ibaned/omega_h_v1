7247d480d07a3bd5c5dad32ba5201f2f557aa192
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-23 17:15:47 -0500

    ditch the _fast names and old decl

--- reflect_down.c
+++ reflect_down.c
@@ -26,8 +26,8 @@
 TODO: consolidate this with find_by_verts.h
 */
 
-#define FIND_LOW_FAST(N) \
-LOOP_INOUT static inline unsigned find_low_fast_##N( \
+#define FIND_LOW(N) \
+LOOP_INOUT static inline unsigned find_low_##N( \
     unsigned const* verts_wanted, \
     unsigned const* verts_of_lows, \
     unsigned const* lows_of_verts_offsets, \
@@ -55,10 +55,10 @@ LOOP_INOUT static inline unsigned find_low_fast_##N( \
   assert(0); \
 }
 
-FIND_LOW_FAST(2)
-FIND_LOW_FAST(3)
+FIND_LOW(2)
+FIND_LOW(3)
 
-LOOP_INOUT static inline unsigned find_low_fast(
+LOOP_INOUT static inline unsigned find_low(
     unsigned nverts_wanted,
     unsigned const* verts_wanted,
     unsigned const* verts_of_lows,
@@ -66,14 +66,14 @@ LOOP_INOUT static inline unsigned find_low_fast(
     unsigned const* lows_of_verts)
 {
   if (nverts_wanted == 2)
-    return find_low_fast_2(verts_wanted, verts_of_lows, lows_of_verts_offsets,
+    return find_low_2(verts_wanted, verts_of_lows, lows_of_verts_offsets,
         lows_of_verts);
   else
-    return find_low_fast_3(verts_wanted, verts_of_lows, lows_of_verts_offsets,
+    return find_low_3(verts_wanted, verts_of_lows, lows_of_verts_offsets,
         lows_of_verts);
 }
 
-LOOP_KERNEL(reflect_down_entity_fast,
+LOOP_KERNEL(reflect_down_entity,
     unsigned const* verts_of_highs,
     unsigned const* verts_of_lows,
     unsigned const* lows_of_verts_offsets,
@@ -91,12 +91,12 @@ LOOP_KERNEL(reflect_down_entity_fast,
     unsigned verts_wanted[3];
     for (unsigned k = 0; k < verts_per_low; ++k)
       verts_wanted[k] = verts_of_high[high_verts_of_low[k]];
-    lows_of_high[j] = find_low_fast(verts_per_low, verts_wanted,
+    lows_of_high[j] = find_low(verts_per_low, verts_wanted,
         verts_of_lows, lows_of_verts_offsets, lows_of_verts);
   }
 }
 
-static unsigned* reflect_down_fast(
+static unsigned* reflect_down(
     unsigned high_dim,
     unsigned low_dim,
     unsigned nhighs,
@@ -112,7 +112,7 @@ static unsigned* reflect_down_fast(
   unsigned* lows_of_highs = LOOP_MALLOC(unsigned, nhighs * lows_per_high);
   unsigned const* const* high_verts_of_lows =
     the_canonical_orders[high_dim][low_dim][0];
-  LOOP_EXEC(reflect_down_entity_fast, nhighs,
+  LOOP_EXEC(reflect_down_entity, nhighs,
       verts_of_highs,
       verts_of_lows,
       lows_of_verts_offsets,
@@ -134,6 +134,6 @@ unsigned* mesh_reflect_down(
   unsigned const* verts_of_highs = mesh_ask_down(m, high_dim, 0);
   struct const_up* lows_of_verts = mesh_ask_up(m, 0, low_dim);
   unsigned const* verts_of_lows = mesh_ask_down(m, low_dim, 0);
-  return reflect_down_fast(high_dim, low_dim, nhighs,
+  return reflect_down(high_dim, low_dim, nhighs,
       verts_of_highs, verts_of_lows, lows_of_verts->offsets, lows_of_verts->adj);
 }
