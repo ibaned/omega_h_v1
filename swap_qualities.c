f21d1e9f21ea8092a596daa60d5c039dde9e3501
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-01-24 11:26:04 -0500

    swap_qualities skips over non-owned edges
    we have allowed other quality operations
    to operate on non-owned entities and then
    just ignored their answers, but the ring
    finding algorithm really needs a proper
    cavity around it or it crashes.

--- swap_qualities.c
+++ swap_qualities.c
@@ -5,6 +5,7 @@
 #include "edge_swap.h"
 #include "loop.h"
 #include "mesh.h"
+#include "parallel_mesh.h"
 #include "quality.h"
 
 static void swap_qualities(
@@ -17,6 +18,7 @@ static void swap_qualities(
     unsigned const* verts_of_tets,
     double const* coords,
     double const* elem_quals,
+    unsigned const* owned_edges,
     double** p_qualities,
     unsigned** p_ring_sizes)
 {
@@ -24,6 +26,8 @@ static void swap_qualities(
   unsigned* ring_sizes = LOOP_MALLOC(unsigned, nedges);
   for (unsigned i = 0; i < nedges; ++i) {
     if (!candidates[i])
+      continue;
+    if (owned_edges && !owned_edges[i])
       continue;
     unsigned first_use = tets_of_edges_offsets[i];
     unsigned end_use = tets_of_edges_offsets[i + 1];
@@ -78,10 +82,14 @@ void mesh_swap_qualities(
   unsigned const* verts_of_tets = mesh_ask_down(m, 3, 0);
   double const* coords = mesh_find_tag(m, 0, "coordinates")->d.f64;
   double* elem_quals = mesh_qualities(m);
+  unsigned* owned_edges = 0;
+  if (mesh_is_parallel(m))
+    owned_edges = mesh_get_owned(m, 1);
   swap_qualities(nedges, candidates,
       tets_of_edges_offsets, tets_of_edges, tets_of_edges_directions,
       verts_of_edges, verts_of_tets,
-      coords, elem_quals,
+      coords, elem_quals, owned_edges,
       p_qualities, p_ring_sizes);
   loop_free(elem_quals);
+  loop_free(owned_edges);
 }
