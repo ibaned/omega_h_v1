59fac81e069a165637bc98106e76a719577e036e
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-02-03 13:24:19 -0500

    loop kernel for collapses_to_ents

--- collapses_to_ents.c
+++ collapses_to_ents.c
@@ -6,6 +6,59 @@
 #include "mesh.h"
 #include "tables.h"
 
+LOOP_KERNEL(collapse_to_ent,
+    unsigned const* verts_of_ents,
+    unsigned verts_per_ent,
+    unsigned sides_per_ent,
+    unsigned const* gen_offset_of_verts,
+    unsigned const* gen_vert_of_verts,
+    unsigned* ent_sides_opp_verts,
+    unsigned const* sides_of_ents,
+    unsigned const* fused_ents,
+    unsigned* ent_is_same,
+    unsigned* fused_sides,
+    unsigned* ent_will_gen,
+    unsigned* gen_vert_of_ents,
+    unsigned* gen_direction_of_ents)
+
+  unsigned const* verts_of_ent = verts_of_ents + i * verts_per_ent;
+  unsigned col_vert = INVALID;
+  unsigned direction = INVALID;
+  for (unsigned j = 0; j < verts_per_ent; ++j) {
+    unsigned vert = verts_of_ent[j];
+    if (gen_offset_of_verts[vert] != gen_offset_of_verts[vert + 1]) {
+      col_vert = vert;
+      direction = j;
+    }
+  }
+  if (direction == INVALID) {
+    ent_is_same[i] = 1;
+    /* not adjacent to collapsing vertex */
+    return;
+  }
+  ent_is_same[i] = 0;
+  unsigned gen_vert = gen_vert_of_verts[col_vert];
+  unsigned gen_dir = INVALID;
+  for (unsigned j = 0; j < verts_per_ent; ++j)
+    if (verts_of_ent[j] == gen_vert)
+      gen_dir = j;
+  unsigned ent_will_die = (gen_dir != INVALID);
+  if (ent_will_die) {
+    if (fused_sides) {
+      unsigned ent_side = ent_sides_opp_verts[gen_dir];
+      unsigned side = sides_of_ents[i * sides_per_ent + ent_side];
+      fused_sides[side] = 1;
+    }
+    return;
+  }
+  unsigned is_fused_side = (fused_ents && fused_ents[i]);
+  if (!is_fused_side) {
+    ent_will_gen[i] = 1;
+    gen_vert_of_ents[i] = gen_vert;
+    gen_direction_of_ents[i] = direction;
+  }
+}
+
 void collapses_to_ents(
     struct mesh* m,
     unsigned ent_dim,
@@ -33,45 +86,23 @@ void collapses_to_ents(
   unsigned* gen_vert_of_ents = LOOP_MALLOC(unsigned, nents);
   unsigned* gen_direction_of_ents = LOOP_MALLOC(unsigned, nents);
   unsigned* ent_is_same = LOOP_MALLOC(unsigned, nents);
-  unsigned const* ent_sides_opp_verts = the_opposite_orders[ent_dim][0];
-  for (unsigned i = 0; i < nents; ++i) {
-    unsigned const* verts_of_ent = verts_of_ents + i * verts_per_ent;
-    unsigned col_vert = INVALID;
-    unsigned direction = INVALID;
-    for (unsigned j = 0; j < verts_per_ent; ++j) {
-      unsigned vert = verts_of_ent[j];
-      if (gen_offset_of_verts[vert] != gen_offset_of_verts[vert + 1]) {
-        col_vert = vert;
-        direction = j;
-      }
-    }
-    if (direction == INVALID) {
-      ent_is_same[i] = 1;
-      /* not adjacent to collapsing vertex */
-      continue;
-    }
-    ent_is_same[i] = 0;
-    unsigned gen_vert = gen_vert_of_verts[col_vert];
-    unsigned gen_dir = INVALID;
-    for (unsigned j = 0; j < verts_per_ent; ++j)
-      if (verts_of_ent[j] == gen_vert)
-        gen_dir = j;
-    unsigned ent_will_die = (gen_dir != INVALID);
-    if (ent_will_die) {
-      if (p_fused_sides) {
-        unsigned ent_side = ent_sides_opp_verts[gen_dir];
-        unsigned side = sides_of_ents[i * sides_per_ent + ent_side];
-        fused_sides[side] = 1;
-      }
-      continue;
-    }
-    unsigned is_fused_side = (fused_ents && fused_ents[i]);
-    if (!is_fused_side) {
-      ent_will_gen[i] = 1;
-      gen_vert_of_ents[i] = gen_vert;
-      gen_direction_of_ents[i] = direction;
-    }
-  }
+  unsigned* ent_sides_opp_verts = LOOP_TO_DEVICE(unsigned,
+      the_opposite_orders[ent_dim][0], verts_per_ent);
+  LOOP_EXEC(collapse_to_ent, nents,
+      verts_of_ents,
+      verts_per_ent,
+      sides_per_ent,
+      gen_offset_of_verts,
+      gen_vert_of_verts,
+      ent_sides_opp_verts,
+      sides_of_ents,
+      fused_ents,
+      ent_is_same,
+      fused_sides,
+      ent_will_gen,
+      gen_vert_of_ents,
+      gen_direction_of_ents);
+  loop_free(ent_sides_opp_verts);
   *p_gen_offset_of_ents = uints_exscan(ent_will_gen, nents);
   loop_free(ent_will_gen);
   *p_gen_vert_of_ents = gen_vert_of_ents;
