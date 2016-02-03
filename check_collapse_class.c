1968515cc5dcb21c36b06f95c9765c3e710d3f9a
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-02-03 13:53:24 -0500

    loop kernel for check_collapse_class

--- check_collapse_class.c
+++ check_collapse_class.c
@@ -16,6 +16,33 @@
    |0/
    |/<-collapse */
 
+LOOP_KERNEL(check_edge_collapse_class,
+    unsigned const* verts_of_edges,
+    unsigned const* class_dim_of_verts,
+    unsigned const* class_dim_of_edges,
+    unsigned* col_codes)
+
+  if (col_codes[i] == DONT_COLLAPSE)
+    return;
+  unsigned const* verts_of_edge = verts_of_edges + i * 2;
+  unsigned class_dims[3];
+  enum { V0, V1, E };
+  class_dims[V0] = class_dim_of_verts[verts_of_edge[0]];
+  class_dims[V1] = class_dim_of_verts[verts_of_edge[1]];
+  class_dims[E] = class_dim_of_edges[i];
+  if (class_dims[V0] == class_dims[V1]) {
+    if (class_dims[V0] != class_dims[E])
+      col_codes[i] = DONT_COLLAPSE;
+    return;
+  }
+  for (unsigned j = 0; j < 2; ++j) {
+    if (!collapses(col_codes[i], j))
+      continue;
+    if (class_dims[j] != class_dims[E])
+      col_codes[i] = dont_collapse(col_codes[i], j);
+  }
+}
+
 static void check_full_collapse_class(struct mesh* m, unsigned* col_codes)
 {
   assert(mesh_get_rep(m) == MESH_FULL);
@@ -25,27 +52,11 @@ static void check_full_collapse_class(struct mesh* m, unsigned* col_codes)
       m, 0, "class_dim")->d.u32;
   unsigned const* class_dim_of_edges = mesh_find_tag(
       m, 1, "class_dim")->d.u32;
-  for (unsigned i = 0; i < nedges; ++i) {
-    if (col_codes[i] == DONT_COLLAPSE)
-      continue;
-    unsigned const* verts_of_edge = verts_of_edges + i * 2;
-    unsigned class_dims[3];
-    enum { V0, V1, E };
-    class_dims[V0] = class_dim_of_verts[verts_of_edge[0]];
-    class_dims[V1] = class_dim_of_verts[verts_of_edge[1]];
-    class_dims[E] = class_dim_of_edges[i];
-    if (class_dims[V0] == class_dims[V1]) {
-      if (class_dims[V0] != class_dims[E])
-        col_codes[i] = DONT_COLLAPSE;
-      continue;
-    }
-    for (unsigned j = 0; j < 2; ++j) {
-      if (!collapses(col_codes[i], j))
-        continue;
-      if (class_dims[j] != class_dims[E])
-        col_codes[i] = dont_collapse(col_codes[i], j);
-    }
-  }
+  LOOP_EXEC(check_edge_collapse_class, nedges,
+      verts_of_edges,
+      class_dim_of_verts,
+      class_dim_of_edges,
+      col_codes);
 }
 
 void check_collapse_class(struct mesh* m, unsigned* col_codes)
