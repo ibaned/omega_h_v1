f18d4cf90ee206fc68efeb609288312542582356
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-02-03 16:50:17 -0500

    loop kernel for rebuilding collapse destinations

--- collapses_to_verts.c
+++ collapses_to_verts.c
@@ -67,6 +67,41 @@ void valid_collapses_to_verts(struct mesh* m)
   mesh_add_tag(m, 0, TAG_F64, "col_qual", 1, col_qual_of_verts);
 }
 
+LOOP_KERNEL(vertex_dest,
+    unsigned const* edges_of_verts_offsets,
+    unsigned const* edges_of_verts,
+    unsigned const* edges_of_verts_directions,
+    unsigned const* verts_of_edges,
+    unsigned const* indset,
+    double const* col_qual_of_verts,
+    unsigned const* col_codes,
+    double const* col_quals_of_edges,
+    unsigned* gen_vert_of_verts)
+  if (!indset[i])
+    return;
+  unsigned first_use = edges_of_verts_offsets[i];
+  unsigned end_use = edges_of_verts_offsets[i + 1];
+  double maxq = col_qual_of_verts[i];
+  if (maxq == 1e10)
+    maxq = -1e10;
+  unsigned gen_vert = INVALID;
+  for (unsigned j = first_use; j < end_use; ++j) {
+    unsigned edge = edges_of_verts[j];
+    unsigned direction = edges_of_verts_directions[j];
+    if (!collapses(col_codes[edge], direction))
+      continue;
+    double q = col_quals_of_edges[edge * 2 + direction];
+    if (q == 1e10)
+      q = -1e10;
+    if (q == maxq) {
+      gen_vert = verts_of_edges[edge * 2 + (1 - direction)];
+      break;
+    }
+  }
+  assert(gen_vert != INVALID);
+  gen_vert_of_verts[i] = gen_vert;
+}
+
 unsigned* collapsing_vertex_destinations(struct mesh* m)
 {
   unsigned const* col_codes = mesh_find_tag(m, 1, "col_codes")->d.u32;
@@ -84,30 +119,15 @@ unsigned* collapsing_vertex_destinations(struct mesh* m)
   unsigned const* edges_of_verts_directions =
     mesh_ask_up(m, 0, 1)->directions;
   unsigned* gen_vert_of_verts = LOOP_MALLOC(unsigned, nverts);
-  for (unsigned i = 0; i < nverts; ++i) {
-    if (!indset[i])
-      continue;
-    unsigned first_use = edges_of_verts_offsets[i];
-    unsigned end_use = edges_of_verts_offsets[i + 1];
-    double maxq = col_qual_of_verts[i];
-    if (maxq == 1e10)
-      maxq = -1e10;
-    unsigned gen_vert = INVALID;
-    for (unsigned j = first_use; j < end_use; ++j) {
-      unsigned edge = edges_of_verts[j];
-      unsigned direction = edges_of_verts_directions[j];
-      if (!collapses(col_codes[edge], direction))
-        continue;
-      double q = col_quals_of_edges[edge * 2 + direction];
-      if (q == 1e10)
-        q = -1e10;
-      if (q == maxq) {
-        gen_vert = verts_of_edges[edge * 2 + (1 - direction)];
-        break;
-      }
-    }
-    assert(gen_vert != INVALID);
-    gen_vert_of_verts[i] = gen_vert;
-  }
+  LOOP_EXEC(vertex_dest, nverts,
+      edges_of_verts_offsets,
+      edges_of_verts,
+      edges_of_verts_directions,
+      verts_of_edges,
+      indset,
+      col_qual_of_verts,
+      col_codes,
+      col_quals_of_edges,
+      gen_vert_of_verts);
   return gen_vert_of_verts;
 }
