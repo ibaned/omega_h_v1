a93fd0ef17855b2346d59059cfc629b5a92ef283
Ben Mizera <mizerb@rpi.edu>
2016-02-11 09:10:03 -0500

    Changes to tables.h

--- tables.h
+++ tables.h
@@ -5,13 +5,13 @@
 #define MAX_UP 128
 #define INVALID (~((unsigned)0))
 
-extern unsigned const* const the_box_conns[4];
-extern double const* const the_box_coords[4];
-extern unsigned const the_box_nelems[4];
-extern unsigned const the_box_nverts[4];
-extern unsigned const the_down_degrees[4][4];
-extern unsigned const* const* const* const* const the_canonical_orders[4];
-extern unsigned const* const* const the_opposite_orders[4];
+LOOP_CONST extern unsigned const* const the_box_conns[4];
+LOOP_CONST extern double const* const the_box_coords[4];
+LOOP_CONST extern unsigned const the_box_nelems[4];
+LOOP_CONST extern unsigned const the_box_nverts[4];
+LOOP_CONST extern unsigned const the_down_degrees[4][4];
+LOOP_CONST extern unsigned const* const* const* const* const the_canonical_orders[4];
+LOOP_CONST extern unsigned const* const* const the_opposite_orders[4];
 
 static inline unsigned get_opposite_dim(
     unsigned elem_dim,
