c1d56254f0a50e3aefd260e1ae4c1f33dacdd954
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-02-12 18:14:11 -0500

    fix tests a bit

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,8 @@ if [ "$LOOP_MODE" = "cuda" ]; then
 fi
 $VALGRIND ./bin/box.exe --file ./scratch/box.vtu --dim 2 --refinements 6
 $VALGRIND ./bin/vtkdiff.exe ./scratch/box.vtu ./scratch/box.vtu
-$VALGRIND ./bin/node_ele.exe ./scratch/xgc.node ./scratch/xgc.ele ./scratch/xgc.vtu
-$VALGRIND ./bin/from_gmsh.exe ./scratch/cube.msh ./scratch/cube.vtu
+$VALGRIND ./bin/node_ele.exe ./data/xgc.node ./data/xgc.ele ./scratch/xgc.vtu
+$VALGRIND ./bin/from_gmsh.exe ./data/cube.msh ./scratch/cube.vtu
 if [ "$USE_MPI" = "1" ]; then
   $MPIRUN -np 2 $VALGRIND ./bin/migrate.exe
   $MPIRUN -np 2 $VALGRIND ./bin/conform.exe
@@ -18,15 +18,15 @@ if [ "$USE_MPI" = "1" ]; then
   $MPIRUN -np 2 $VALGRIND ./bin/one_coarsen.exe ./scratch/one_cor.pvtu ./scratch/two_cor.pvtu
 fi
 $VALGRIND ./bin/warp.exe
-if [ -e ./gold/2d_warp_0008.vtu ]
-  ./bin/vtkdiff.exe warp_0008.vtu ./gold/2d_warp_0008.vtu
+if [ -e ./gold/2d_warp_0008.vtu ]; then
+  $VALGRIND ./bin/vtkdiff.exe warp_0008.vtu ./gold/2d_warp_0008.vtu
   diff warp_0008.vtu ./gold/2d_warp_0008.vtu
 fi
 cp warp_0008.vtu ./gold/2d_warp_0008.vtu
 if [ "$PATIENT" = "1" ]; then
   $VALGRIND ./bin/warp_3d.exe
-  if [ -e ./gold/warp_0016.vtu ]
-    ./bin/vtkdiff.exe warp_0016.vtu ./gold/warp_0016.vtu
+  if [ -e ./gold/warp_0016.vtu ]; then
+    $VALGRIND ./bin/vtkdiff.exe warp_0016.vtu ./gold/warp_0016.vtu
     diff warp_0016.vtu ./gold/warp_0016.vtu
   fi
   cp warp_0016.vtu ./gold/warp_0016.vtu
