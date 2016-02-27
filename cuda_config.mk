b32d4d8d78ef35a749cf83e04e2255944054229e
Benjamin Mizera <mizerb@rpi.edu>
2016-02-27 10:59:24 -0500

    THis is the correct config for me

--- cuda_config.mk
+++ cuda_config.mk
@@ -1,6 +1,6 @@
 CPP = nvcc
 CC = nvcc
 CPPFLAGS = -x cu
-CFLAGS = -g -O2 --gpu-code=sm_37 --gpu-architecture=compute_37 -rdc true
-LDFLAGS = --gpu-code=sm_37 --gpu-architecture=compute_37 -rdc true
+CFLAGS = -g -O2 --gpu-code=sm_30 --gpu-architecture=compute_30 -rdc true
+LDFLAGS = --gpu-code=sm_30 --gpu-architecture=compute_30 -rdc true
 LOOP_MODE = cuda
