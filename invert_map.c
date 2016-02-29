3fd63fe8039cd493cf878592b22d2ea09b5e3dcc
Ben Mizera <mizerb@rpi.edu>
2016-02-28 20:56:08 -0500

    Readded the serial code with pre processor switch

--- invert_map.c
+++ invert_map.c
@@ -4,7 +4,11 @@
 #include "ints.h"
 #include "loop.h"
 
+
+#ifdef __CUDACC__
+
 #include <thrust/sort.h>
+#include <thrust/device_ptr.h>
 
 struct Counter
 {
@@ -30,9 +34,6 @@ LOOP_KERNEL( assign ,const unsigned * in, struct Counter * ref )
   ref[i].origin = i;
   ref[i].value  = in[i];
 }
-
-#include <thrust/device_vector.h>
-#include <thrust/fill.h>
 
 __host__ __device__ bool operator<(const Counter &A, const Counter &B)
 {
@@ -62,3 +63,72 @@ void invert_map(
   *p_out = out;
   *p_offsets = aoffsets;
 }
+
+#else
+
+LOOP_KERNEL(count,
+    unsigned nout,
+    unsigned const* in,
+    unsigned* counts)
+  unsigned d = in[i];
+  assert(d < nout);
+  loop_atomic_increment(&(counts[d]));
+}
+
+LOOP_KERNEL(fill,
+    unsigned const* in,
+    unsigned* offsets,
+    unsigned* counts,
+    unsigned* out)
+  unsigned d = in[i];
+  unsigned o = offsets[d];
+  unsigned j = loop_atomic_increment(&(counts[d]));
+  out[o + j] = i;
+}
+
+/* the atomic increments give the fill
+   kernel some non-determinism,
+   which we can counteract by sorting the sub-arrays.
+   given the assumption that these sub-arrays are small
+   (about a dozen entries),
+   we use a hand-coded selection sort.
+   there is a reward for someone who comes up with
+   an equally efficient implementation that is deterministic
+   from the start */
+/*
+LOOP_KERNEL(sort,
+    unsigned* offsets,
+    unsigned* out)
+  unsigned first = offsets[i];
+  unsigned end = offsets[i + 1];
+  for (unsigned j = first; j < end; ++j) {
+    unsigned min_k = j;
+    for (unsigned k = j + 1; k < end; ++k)
+      if (out[k] < out[min_k])
+        min_k = k;
+    unsigned tmp = out[j];
+    out[j] = out[min_k];
+    out[min_k] = tmp;
+  }
+}
+*/
+void invert_map(
+    unsigned nin,
+    unsigned const* in,
+    unsigned nout,
+    unsigned** p_out,
+    unsigned** p_offsets)
+{
+  unsigned* counts = uints_filled(nout, 0);
+  LOOP_EXEC(count, nin, nout, in, counts);
+  unsigned* offsets = uints_exscan(counts, nout);
+  unsigned* out = LOOP_MALLOC(unsigned, nin);
+  loop_free(counts);
+  counts = uints_filled(nout, 0);
+  LOOP_EXEC(fill, nin, in, offsets, counts, out);
+  loop_free(counts);
+  LOOP_EXEC(sort, nout, offsets, out);
+  *p_out = out;
+  *p_offsets = offsets;
+}
+#endif
