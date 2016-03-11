fdf8f7da735251197f26ab521c22973e440e6a62
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-03-11 12:13:28 -0500

    simply kokkos_config.mk

--- kokkos_config.mk
+++ kokkos_config.mk
@@ -1,14 +1,3 @@
-CXX = mpicxx
-CXXFLAGS = -g -O2 -Werror -Weverything -Wno-c++98-compat-pedantic -Wno-float-equal -Wno-padded
-KOKKOS_PREFIX = /Users/dibanez/code/kokkos-install
+CXXFLAGS = -g -O2 -Werror -Wall
+KOKKOS_MAKEFILE = /Users/dibanez/code/kokkos-install/Makefile.kokkos
 LOOP_MODE = kokkos
-USE_ZLIB = 1
-USE_MPI = 1
-USE_MPI3 = 1
-PATIENT = 1
-LDLIBS = -lz
-VALGRIND = "valgrind --leak-check=full --error-exitcode=1"
-CPPFLAGS += -std=c++11
-CPPFLAGS += -I$(KOKKOS_PREFIX)/include
-LDFLAGS += -L$(KOKKOS_PREFIX)/lib
-LDLIBS += -lkokkos
