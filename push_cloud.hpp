9f873d9bdadb9d8cc90a7002682ec56ba60216cf
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-03-12 13:11:40 -0500

    removing all traces of "cloud" code
    when/if we get back to particles,
    we'll call them that.

--- push_cloud.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PUSH_CLOUD_HPP
-#define PUSH_CLOUD_HPP
-
-struct cloud;
-struct mesh;
-
-void push_cloud(struct cloud* c, struct mesh* m);
-
-#endif
--- vtk_io.hpp
+++ vtk_io.hpp
@@ -2,7 +2,6 @@
 #define VTK_IO_HPP
 
 struct mesh;
-struct cloud;
 
 enum vtk_format {
   VTK_ASCII,
@@ -18,16 +17,5 @@ void write_mesh_vtk_opts(struct mesh* m, char const* outpath,
 
 void start_vtk_steps(char const* prefix);
 void write_vtk_step(struct mesh* m);
-
-void write_vtu_cloud_opts(struct cloud* c, char const* filename,
-    enum vtk_format fmt);
-void write_vtu_cloud(struct cloud* c, char const* filename);
-struct cloud* read_vtu_cloud(char const* filename);
-
-void write_pvtu_cloud(struct cloud* c, char const* filename,
-    unsigned npieces);
-
-struct cloud* read_parallel_vtu_cloud(char const* inpath);
-void write_parallel_vtu_cloud(struct cloud* c, char const* outpath);
 
 #endif
