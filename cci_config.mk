0e85ffc7434fbca134f562c7810368dc9fd357b3
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-03-13 20:55:55 -0400

    update cci_config

--- cci_config.mk
+++ cci_config.mk
@@ -1,8 +1,6 @@
 #module load gnu-4.7.2
-CC = mpicc
-CPP = mpicc
-CFLAGS = -O2 -Wall -Wextra -pedantic -std=c99
-LDFLAGS = -O2
+CXX = mpicxx
+CXXFLAGS = -O2 -Wall -Wextra -pedantic -std=c++11
 LDLIBS = -lm
 USE_MPI = 1
 # CCI's MPI library is older than MPI3.
