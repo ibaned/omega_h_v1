360f82dcaccd113dc46b02ab2f5b44f1fa733a8b
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-04-19 15:17:03 -0400

    draft check for collapsing exposed sides

--- check_collapse_class.cpp
+++ check_collapse_class.cpp
@@ -9,16 +9,7 @@
 
 namespace omega_h {
 
-/* for now we'll just do the simple check and skip
-   things like rings and exposed curved faces */
-/* TODO: at least check the exposed face issue:
-
-   |\ R1
-   |R\
-   |0/
-   |/<-collapse */
-
-LOOP_KERNEL(check_edge_collapse_class,
+LOOP_KERNEL(check_basic_edge_class,
     unsigned const* verts_of_edges,
     unsigned const* class_dim_of_verts,
     unsigned const* class_dim_of_edges,
@@ -45,7 +36,7 @@ LOOP_KERNEL(check_edge_collapse_class,
   }
 }
 
-static void check_full_collapse_class(struct mesh* m, unsigned* col_codes)
+static void check_basic_class(struct mesh* m, unsigned* col_codes)
 {
   assert(mesh_get_rep(m) == MESH_FULL);
   unsigned nedges = mesh_count(m, 1);
@@ -54,16 +45,103 @@ static void check_full_collapse_class(struct mesh* m, unsigned* col_codes)
       m, 0, "class_dim")->d.u32;
   unsigned const* class_dim_of_edges = mesh_find_tag(
       m, 1, "class_dim")->d.u32;
-  LOOP_EXEC(check_edge_collapse_class, nedges,
+  LOOP_EXEC(check_basic_edge_class, nedges,
       verts_of_edges,
       class_dim_of_verts,
       class_dim_of_edges,
       col_codes);
 }
 
+LOOP_KERNEL(check_for_exposed_side,
+    unsigned ent_dim,
+    unsigned const* ents_of_edges_offsets,
+    unsigned const* ents_of_edges,
+    unsigned const* ents_of_edges_directions,
+    unsigned const* verts_of_ents,
+    unsigned nverts_per_ent,
+    unsigned const* verts_of_edges,
+    unsigned const* sides_of_ents,
+    unsigned nsides_per_ent,
+    unsigned const* class_dim_of_ents,
+    unsigned const* class_dim_of_sides,
+    unsigned* col_codes)
+
+  if (col_codes[i] == DONT_COLLAPSE)
+    return;
+  unsigned const* const* ent_verts_of_edges =
+    the_canonical_orders[ent_dim][1][0];
+  unsigned const* ent_sides_opp_verts =
+    the_opposite_orders[ent_dim][0];
+  unsigned f = ents_of_edges_offsets[i];
+  unsigned e = ents_of_edges_offsets[i + 1];
+  for (unsigned j = f; j < e; ++j) {
+    unsigned ent = ents_of_edges[j];
+    unsigned dir = ents_of_edges_directions[j];
+    unsigned ent_class_dim = class_dim_of_ents[ent];
+    unsigned const* ent_verts_of_edge =
+      ent_verts_of_edges[dir];
+    for (unsigned k = 0; k < 2; ++k) {
+      unsigned eev = ent_verts_of_edge[k];
+      unsigned va = verts_of_ents[ent * nverts_per_ent + eev];
+      unsigned l;
+      for (l = 0; l < 2; ++l) {
+        unsigned vb = verts_of_edges[i * 2 + l];
+        if (va == vb) {
+          unsigned es = ent_sides_opp_verts[eev];
+          unsigned side = sides_of_ents[ent * nsides_per_ent + es];
+          unsigned side_class_dim = class_dim_of_sides[side];
+          if (side_class_dim != ent_class_dim)
+            col_codes[i] = dont_collapse(col_codes[i], l);
+          break;
+        }
+      }
+      assert(l != 2);
+    }
+  }
+}
+
+static void check_for_exposed_sides(struct mesh* m, unsigned ent_dim,
+    unsigned* col_codes)
+{
+  unsigned const* ents_of_edges_offsets =
+    mesh_ask_up(m, 1, ent_dim)->offsets;
+  unsigned const* ents_of_edges =
+    mesh_ask_up(m, 1, ent_dim)->adj;
+  unsigned const* ents_of_edges_directions =
+    mesh_ask_up(m, 1, ent_dim)->directions;
+  unsigned const* verts_of_ents =
+    mesh_ask_down(m, ent_dim, 0);
+  unsigned nverts_per_ent = the_down_degrees[ent_dim][0];
+  unsigned const* verts_of_edges =
+    mesh_ask_down(m, 1, 0);
+  unsigned const* sides_of_ents =
+    mesh_ask_down(m, ent_dim, ent_dim - 1);
+  unsigned nsides_per_ent = the_down_degrees[ent_dim][ent_dim - 1];
+  unsigned const* class_dim_of_ents = mesh_find_tag(
+      m, ent_dim, "class_dim")->d.u32;
+  unsigned const* class_dim_of_sides = mesh_find_tag(
+      m, ent_dim - 1, "class_dim")->d.u32;
+  unsigned nedges = mesh_count(m, 1);
+  LOOP_EXEC(check_for_exposed_side, nedges,
+      ent_dim,
+      ents_of_edges_offsets,
+      ents_of_edges,
+      ents_of_edges_directions,
+      verts_of_ents,
+      nverts_per_ent,
+      verts_of_edges,
+      sides_of_ents,
+      nsides_per_ent,
+      class_dim_of_ents,
+      class_dim_of_sides,
+      col_codes);
+}
+
 void check_collapse_class(struct mesh* m, unsigned* col_codes)
 {
-  check_full_collapse_class(m, col_codes);
+  check_basic_class(m, col_codes);
+  for (unsigned d = 2; d <= mesh_dim(m); ++d)
+    check_for_exposed_sides(m, d, col_codes);
 }
 
 }
