c7742e1230834f37ff812278976c2222bb5da3fd
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-06 10:21:07 -0400

    timer utilities

---
+++ timer.hpp
@@ -0,0 +1,23 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+
+namespace omega_h {
+
+typedef std::chrono::time_point<std::chrono::high_resolution_clock> Now;
+
+static Now now()
+{
+  return std::chrono::high_resolution_clock::now();
+}
+
+static double seconds_between(Now a, Now b)
+{
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(b - a).count() * 1e-9;
+}
+
+}
+
+#endif
+
