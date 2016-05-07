c06a9aeec1677372ea2214e38d9ce21a422b09db
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-07 11:26:27 -0400

    drafted convergence check for smoother

--- smooth.cpp
+++ smooth.cpp
@@ -4,7 +4,9 @@
 
 #include "algebra.hpp"
 #include "arrays.hpp"
+#include "comm.hpp"
 #include "ghost_mesh.hpp"
+#include "ints.hpp"
 #include "loop.hpp"
 #include "mark.hpp"
 #include "mesh.hpp"
@@ -38,12 +40,21 @@ LOOP_KERNEL(smooth_field_vert,
       data_out + i * ncomps, ncomps);
 }
 
-static void smooth_field_iter(
+LOOP_KERNEL(check_diff,
+    double const* a,
+    double const* b,
+    double tol,
+    unsigned* diffs)
+  diffs[i] = (fabs(a[i] - b[i]) > tol);
+}
+
+static unsigned smooth_field_iter(
     unsigned n,
     unsigned ncomps,
     unsigned const* interior,
     unsigned const* star_offsets,
     unsigned const* star,
+    double tol,
     double** p_data)
 {
   double* data_in = *p_data;
@@ -52,6 +63,12 @@ static void smooth_field_iter(
       data_in, data_out);
   loop_free(data_in);
   *p_data = data_out;
+  unsigned* diffs = LOOP_MALLOC(unsigned, n * ncomps);
+  LOOP_EXEC(check_diff, n * ncomps, data_in, data_out, tol, diffs);
+  unsigned not_done = uints_max(diffs, n * ncomps);
+  loop_free(diffs);
+  not_done = comm_max_uint(not_done);
+  return not_done;
 }
 
 unsigned mesh_smooth_field(struct mesh* m, char const* name,
@@ -69,12 +86,18 @@ unsigned mesh_smooth_field(struct mesh* m, char const* name,
   unsigned* interior = mesh_mark_class(m, 0, mesh_dim(m), INVALID);
   unsigned i;
   for (i = 0; i < maxiter; ++i) {
-    smooth_field_iter(n, ncomps, interior, star_offsets, star, &data);
+    unsigned not_done = smooth_field_iter(
+        n, ncomps, interior, star_offsets, star, tol, &data);
     mesh_conform_array(m, 0, ncomps, &data);
+    if (!not_done)
+      break;
   }
-  printf("smoothing converged in %u iterations\n", i);
   loop_free(interior);
   mesh_add_tag(m, 0, TAG_F64, name, ncomps, data);
+  if (i == maxiter)
+    printf("smoothing failed to converge after %u iterations\n", i);
+  else
+    printf("smoothing converged in %u iterations\n", i);
   return i;
 }
 
