7c6fc93cd83a2f20abd2bb6abfeb3861e2f2e942
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-07 11:26:44 -0400

    using closure marking for imr test

--- test_imr.cpp
+++ test_imr.cpp
@@ -2,6 +2,7 @@
 
 #include "arrays.hpp"
 #include "include/omega_h.h"
+#include "mark.hpp"
 #include "mesh.hpp"
 #include "coarsen.hpp"
 #include "vtk_io.hpp"
@@ -11,29 +12,19 @@
 
 using namespace omega_h;
 
-LOOP_KERNEL(move_vert,
-    unsigned const* class_id,
+LOOP_KERNEL(move_object_vert,
+    unsigned const* object_verts,
     double* coords)
-  switch (class_id[i]) {
-    case 12:
-    case 13:
-    case 14:
-    case 15:
-    case 17:
-    case 18:
-    case 19:
-    case 20:
-    case 21:
-      coords[i * 3 + 0] += 0.02;
-  }
+  if (object_verts[i])
+    coords[i * 3 + 0] += 0.02;
 }
 
 static void move_mesh(struct mesh* m)
 {
   unsigned nverts = mesh_count(m, 0);
-  unsigned const* class_id = mesh_find_tag(m, 0, "class_id")->d.u32;
+  unsigned* object_verts = mesh_mark_class_closure_verts(m, 2, 21);
   double* coords = mesh_find_tag(m, 0, "coordinates")->d.f64;
-  LOOP_EXEC(move_vert, nverts, class_id, coords);
+  LOOP_EXEC(move_object_vert, nverts, object_verts, coords);
   mesh_smooth_field(m, "coordinates", 0.01, 50);
 }
 
@@ -45,6 +36,7 @@ int main(int argc, char** argv)
   mesh_add_tag(m, 0, TAG_F64, "adapt_size", 1,
       filled_array(mesh_count(m, 0), 0.1));
   start_vtk_steps("imr");
+  write_vtk_step(m);
   for (unsigned i = 0; i < 1; ++i) {
     move_mesh(m);
   //mesh_adapt(m, 0.3, 0.3, 4, 50);
