9d5bf6b1ab8f9b069ed3779c7887dcb8ad24eabe
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-07 12:28:03 -0400

    adapt during rotation, looks good

--- test_imr.cpp
+++ test_imr.cpp
@@ -59,7 +59,7 @@ int main(int argc, char** argv)
   write_vtk_step(m);
   for (unsigned i = 0; i < 16; ++i) {
     move_mesh(m);
-  //mesh_adapt(m, 0.3, 0.3, 4, 50);
+    mesh_adapt(m, 0.3, 0.3, 4, 50);
     write_vtk_step(m);
   }
   free_mesh(m);
