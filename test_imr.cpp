7e7380cba9bf03f51d983dbc1175939091489c43
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-08 12:53:50 -0400

    3D collide adapt

--- test_imr.cpp
+++ test_imr.cpp
@@ -84,7 +84,7 @@ static void one_motion(struct mesh* m)
   for (unsigned i = 0; i < 3; ++i) {
     printf("sub-warp step %u starting\n", i);
     unsigned done;
-    if ((1)) {
+    if ((0)) {
       done = mesh_warp_to_limit(m, -10.0);
     } else {
       done = mesh_warp_to_limit(m, 0.2);
