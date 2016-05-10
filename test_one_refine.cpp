bf8c23973ddb0e94f827790d706492aa33afe85d
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-05-10 11:07:52 -0400

    time one_refine

--- test_one_refine.cpp
+++ test_one_refine.cpp
@@ -1,9 +1,11 @@
 #include <cassert>
+#include <cstdio>
 
 #include "include/omega_h.h"
 #include "mesh.hpp"
 #include "refine.hpp"
 #include "vtk_io.hpp"
+#include "timer.hpp"
 
 using namespace omega_h;
 
@@ -12,7 +14,10 @@ int main(int argc, char** argv)
   assert(argc == 3);
   osh_init(&argc, &argv);
   struct mesh* m = read_mesh_vtk(argv[1]);
+  Now t0 = now();
   uniformly_refine(m);
+  Now t1 = now();
+  printf("uniform refinement took %e seconds\n", seconds_between(t0,t1));
   write_mesh_vtk(m, argv[2]);
   free_mesh(m);
   osh_fini();
